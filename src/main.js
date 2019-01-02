431ebcf00124519a959cefe26f54b1c867e3ed21
Erik Verheul <erik@verheulconsultants.nl>
2019-01-02 09:45:51 +0100

    Remove logging

--- src/main.js
+++ src/main.js
@@ -9,13 +9,9 @@ axios.defaults.baseURL = 'https://localhost:6984'
 //axios.defaults.headers.get['Accepts'] = 'application/json'
 
 const reqInterceptor = axios.interceptors.request.use(config => {
-  // eslint-disable-next-line no-console
-  console.log('Request Interceptor', config)
   return config
 })
 const resInterceptor = axios.interceptors.response.use(res => {
-  // eslint-disable-next-line no-console
-  console.log('Response Interceptor', res)
   return res
 })
 
