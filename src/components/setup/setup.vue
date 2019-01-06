90e9a09e60a6343a9f4a9a22eece32507740ed7b
Erik Verheul <erik@verheulconsultants.nl>
2019-01-06 17:09:48 +0100

    Format html

--- src/components/setup/setup.vue
+++ src/components/setup/setup.vue
@@ -4,127 +4,138 @@
       <div class="col-lg-12 col-sm-12">
         <h4 v-if="name">As database administrator you're authenticated to setup the database for OneBacklog.
           Your user name is: {{ name }}, and your roles are: {{ roles }}</h4>
-          <h6>Note that users with their roles must be created in the protected _users database to sign in.<br/>
+        <h6>Note that users with their roles must be created in the protected _users database to sign in.<br/>
             Create and initialize the database for OneBacklog first. Then can assign known users and/or roles to it.</h6>
-          </div>
-          <div class="col-lg-4 col-sm-4">
-            <b-img :src="require('../../assets/logo.png')" center fluid alt="OneBacklog logo"/>
-          </div>
-          <div class="col-lg-8 col-sm-8">
-            <button @click="showCredentials" class="myButton">0. Show my credentials</button>
-            <button @click="chPassword" class="myButton">1. Change my password</button>
-            <button @click="createUser" class="myButton">2. Create user</button>
-            <button @click="crateDB" class="myButton">3. Choose or Create a database</button>
-            <button @click="initDB" class="myButton">4. Initialize the database</button>
-            <button @click="assignUser" class="myButton">5. Add new users and roles to the last selected || created database</button>
-            <button @click="showDBsecurity" class="myButton">6. Show the database security info</button>
-            <button @click="showDocuments" class="myButton">7. Show the documents in a database</button>
-            <button @click="showDocById" class="myButton">8. Show a document by id</button>
-            <button @click="deleteDB" class="myButton">9. Delete the selected || created database</button>
-            <button @click="exampleDB" class="myButton">10. Create an example database with demo users</button>
-          </div>
+      </div>
+      <div class="col-lg-4 col-sm-4">
+        <b-img :src="require('../../assets/logo.png')" center fluid alt="OneBacklog logo" />
+      </div>
+      <div class="col-lg-8 col-sm-8">
+        <button @click="showCredentials" class="myButton">0. Show my credentials</button>
+        <button @click="chPassword" class="myButton">1. Change my password</button>
+        <button @click="createUser" class="myButton">2. Create user</button>
+        <button @click="crateDB" class="myButton">3. Choose or Create a database</button>
+        <button @click="initDB" class="myButton">4. Initialize the database</button>
+        <button @click="assignUser" class="myButton">5. Add new users and roles to the last selected || created database</button>
+        <button @click="showDBsecurity" class="myButton">6. Show the database security info</button>
+        <button @click="showDocuments" class="myButton">7. Show the documents in a database</button>
+        <button @click="showDocById" class="myButton">8. Show a document by id</button>
+        <button @click="deleteDB" class="myButton">9. Delete the selected || created database</button>
+        <button @click="exampleDB" class="myButton">10. Create an example database with demo users</button>
+      </div>
 
-          <div class="col-lg-12 col-sm-12">
-            <table>
-              <thead>
-                <tr v-if="selectionMade">
-                  <td><strong>Field-1</strong></td>
-                  <td><strong>Field-2</strong></td>
-                  <td><strong>Field-3</strong></td>
-                  <td><strong>Field-4</strong></td>
-                  <td></td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-if="selectionMade">
-                  <td><input type="text" v-model="row.field1"></td>
-                  <td><input type="text" v-model="row.field2"></td>
-                  <td><input type="text" v-model="row.field3"></td>
-                  <td><input type="text" v-model="row.field4"></td>
-                  <td>
-                    <a @click="execute" class="myButton">Execute</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="col-lg-12 col-sm-12">
-            <div class="myBox">
-              <hr>
-              <p>The output from CouchDB:<p>
-                <p style="white-space: pre">{{ message }}</p>
-                <p v-if="comment">Comment: {{ comment }} </p>
-                <p class="p_red" v-if="errorMessage">Error: {{ errorMessage }} </p>
-              </div>
-            </div>
-          </div>
+      <div class="col-lg-12 col-sm-12">
+        <table>
+          <thead>
+            <tr v-if="selectionMade">
+              <td><strong>Field-1</strong></td>
+              <td><strong>Field-2</strong></td>
+              <td><strong>Field-3</strong></td>
+              <td><strong>Field-4</strong></td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-if="selectionMade">
+              <td>
+                <input type="text" v-model="row.field1">
+              </td>
+              <td>
+                <input type="text" v-model="row.field2">
+              </td>
+              <td>
+                <input type="text" v-model="row.field3">
+              </td>
+              <td>
+                <input type="text" v-model="row.field4">
+              </td>
+              <td>
+                <a @click="execute" class="myButton">Execute</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-lg-12 col-sm-12">
+        <div class="myBox">
+          <hr>
+          <p>The output from CouchDB:</p>
+          <p style="white-space: pre">{{ message }}</p>
+          <p v-if="comment">Comment: {{ comment }} </p>
+          <p class="p_red" v-if="errorMessage">Error: {{ errorMessage }} </p>
         </div>
-      </template>
+      </div>
+    </div>
+  </div>
+</template>
 
-      <script>
-      import {mapGetters} from 'vuex'
-      export default {
-        data () {
-          return {
-            selectionMade: false,
-            row: {
-              field1: '',
-              field2: '',
-              field3: '',
-              field4: ''
-            },
-            commandNr: null,
-            currentDb: this.$store.state.currentDb
-          }
-        },
-        computed: {
-          ...mapGetters({
-            name: 'user',
-            roles: 'roles',
-            message: 'returnMessage',
-            comment: 'returnComment',
-            errorMessage: 'returnErrorMsg'
-          }),
-        },
-        created () {
-          //this.$store.dispatch('fetchUser')
-        },
-        methods: {
-          execute() {
+<script>
+  import {
+    mapGetters
+  }
+  from 'vuex'
+  export default {
+    data() {
+        return {
+          selectionMade: false,
+          row: {
+            field1: '',
+            field2: '',
+            field3: '',
+            field4: ''
+          },
+          commandNr: null,
+          currentDb: this.$store.state.currentDb
+        }
+      },
+      computed: {
+        ...mapGetters({
+          name: 'user',
+          roles: 'roles',
+          message: 'returnMessage',
+          comment: 'returnComment',
+          errorMessage: 'returnErrorMsg'
+        }),
+      },
+      created() {
+        //this.$store.dispatch('fetchUser')
+      },
+      methods: {
+        execute() {
             switch (this.commandNr) {
               case 0:
-              this.showCredentialsExe()
-              break
+                this.showCredentialsExe()
+                break
               case 1:
-              this.chPasswordExe()
-              break
+                this.chPasswordExe()
+                break
               case 2:
-              this.createUserExe()
-              break
+                this.createUserExe()
+                break
               case 3:
-              this.crateDBExe()
-              break
+                this.crateDBExe()
+                break
               case 4:
-              this.initDBExe()
-              break
+                this.initDBExe()
+                break
               case 5:
-              this.assignUserExe()
-              break
+                this.assignUserExe()
+                break
               case 6:
-              this.showDBsecurityExe()
-              break
+                this.showDBsecurityExe()
+                break
               case 7:
-              this.showDocumentsExe()
-              break
+                this.showDocumentsExe()
+                break
               case 8:
-              this.showDocByIdExe()
-              break
+                this.showDocByIdExe()
+                break
               case 9:
-              this.deleteDBExe()
-              break
+                this.deleteDBExe()
+                break
               case 10:
-              this.exampleDBExe()
-              break
+                this.exampleDBExe()
+                break
               default:
             }
           },
@@ -320,63 +331,65 @@
             this.$store.dispatch('setSecurity', payload)
             this.$store.dispatch('createExampleDB', payload)
           },
-        }
       }
-      </script>
+  }
+</script>
 
-      <style scoped>
-      h1, h4, h5, h6 {
-        margin: 20px;
-        text-align: left;
-      }
+<style scoped>
+  h1,
+  h4,
+  h5,
+  h6 {
+    margin: 20px;
+    text-align: left;
+  }
 
-      .p_red {
-        color: red;
-      }
+  .p_red {
+    color: red;
+  }
 
-      .myButton {
-        margin-left: 20px;
-        margin-bottom: 10px;
-        background-color: inherit;
-        border: 1px solid black;
-        border-radius: 5px;
-        color: blue;
-        padding: 5px 5px;
-        text-align: center;
-        text-decoration: none;
-        display: block;
-        font-size: 16px;
-        cursor: pointer;
-      }
+  .myButton {
+    margin-left: 20px;
+    margin-bottom: 10px;
+    background-color: inherit;
+    border: 1px solid black;
+    border-radius: 5px;
+    color: blue;
+    padding: 5px 5px;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  /* Box styles */
 
-      /* Box styles */
-      .myBox {
-        margin-left: 20px;
-        border: 2px;
-        padding: 5px;
-        font: 12px/18px sans-serif;
-        height: 300px;
-        overflow: scroll;
-      }
+  .myBox {
+    margin-left: 20px;
+    border: 2px;
+    padding: 5px;
+    font: 12px/18px sans-serif;
+    height: 300px;
+    overflow: scroll;
+  }
+  /* Scrollbar styles */
 
-      /* Scrollbar styles */
-      ::-webkit-scrollbar {
-        width: 12px;
-        height: 12px;
-      }
+  ::-webkit-scrollbar {
+    width: 12px;
+    height: 12px;
+  }
 
-      ::-webkit-scrollbar-track {
-        border: 1px solid yellowgreen;
-        border-radius: 10px;
-      }
+  ::-webkit-scrollbar-track {
+    border: 1px solid yellowgreen;
+    border-radius: 10px;
+  }
 
-      ::-webkit-scrollbar-thumb {
-        background: yellowgreen;
-        border-radius: 10px;
-      }
+  ::-webkit-scrollbar-thumb {
+    background: yellowgreen;
+    border-radius: 10px;
+  }
 
-      ::-webkit-scrollbar-thumb:hover {
-        background: #88ba1c;
-      }
-
-      </style>
+  ::-webkit-scrollbar-thumb:hover {
+    background: #88ba1c;
+  }
+</style>
