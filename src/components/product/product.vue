b176d6148326beca157c9178818668496817e2e0
Erik Verheul <erik@verheulconsultants.nl>
2019-01-08 16:56:07 +0100

    Use deep selectors

--- src/components/product/product.vue
+++ src/components/product/product.vue
@@ -26,9 +26,51 @@
 
       <multipane-resizer></multipane-resizer>
       <div class="pane" :style="{ flexGrow: 1 }">
-        <div>
-          <h6 class="title is-6">Vertical Pane 2</h6>
-        </div>
+        <!-- inner horizontal panes -->
+        <multipane class="horizontal-panes" layout="horizontal">
+          <div class="pane" :style="{ minHeight: '100px', height: '200px', maxHeight: '300px' }">
+            <div>
+              <h6 class="title is-6">Pane 1</h6>
+              <p class="subtitle is-6">Fixed height.</p>
+              <p>
+                <small>
+                <strong>Configured with:</strong><br/>
+                minHeight: 100px<br/>
+                height: 200px<br/>
+                maxHeight: 300px<br/>
+              </small>
+              </p>
+            </div>
+          </div>
+          <multipane-resizer></multipane-resizer>
+          <div class="pane" :style="{ height: '40%', maxHeight: '60%' }">
+            <div>
+              <h6 class="title is-6">Pane 2</h6>
+              <p class="subtitle is-6">Fluid height.</p>
+              <p>
+                <small>
+                <strong>Configured with:</strong><br/>
+                height: 40%<br/>
+                maxHeight: 60%<br/>
+              </small>
+              </p>
+            </div>
+          </div>
+          <multipane-resizer></multipane-resizer>
+          <div class="pane" :style="{ flexGrow: 1 }">
+            <div>
+              <h6 class="title is-6">Pane 3</h6>
+              <p class="subtitle is-6">Takes remaining available space.</p>
+
+              <p>
+                <small>
+                <strong>Configured with:</strong><br/>
+                flex-grow: 1<br/>
+              </small>
+              </p>
+            </div>
+          </div>
+        </multipane>
       </div>
     </multipane>
   </multipane>
@@ -50,14 +92,14 @@
 
 <style lang="css" scoped>
 
-<!-- horizontal panes -->
+  <!-- horizontal panes -->
   .horizontal-panes {
     width: 100%;
     height: 100%;
     border: 1px solid #ccc;
   }
 
-  <!-- use deep selecor here. See https://vue-loader.vuejs.org/guide/scoped-css.html#deep-selectors -->
+  <!-- Use deep selecor here and below. See https://vue-loader.vuejs.org/guide/scoped-css.html#deep-selectors -->
   .horizontal-panes >>> .pane {
     text-align: left;
     padding: 15px;
@@ -65,7 +107,6 @@
     background: #eee;
   }
 
-  <!-- use deep selecor here. See https://vue-loader.vuejs.org/guide/scoped-css.html#deep-selectors -->
   .horizontal-panes >>> .pane ~ .pane {
     border-top: 1px solid #ccc;
   }
@@ -84,7 +125,7 @@
     height: 400px;
   }
 
-  .custom-resizer > .pane {
+  .custom-resizer >>> .pane {
     text-align: left;
     padding: 15px;
     overflow: hidden;
@@ -92,9 +133,9 @@
     border: 1px solid #ccc;
   }
 
-  .custom-resizer > .pane ~ .pane {}
+  .custom-resizer >>> .pane ~ .pane {}
 
-  .custom-resizer > .multipane-resizer {
+  .custom-resizer >>> .multipane-resizer {
     margin: 0;
     left: 0;
     position: relative;
