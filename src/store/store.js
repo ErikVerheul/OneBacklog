8075c1aef5ed18f3aeb8dc001ef5062cff22a49c
Erik Verheul <erik@verheulconsultants.nl>
2019-03-05 10:47:08 +0100

    Refactor config load

--- src/store/store.js
+++ src/store/store.js
@@ -8,165 +8,181 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
 
-  state: {
-    user: null,
-    roles: [],
-    databases: [],
-    currentDb: null,
-    email: null,
-    config: null,
-    runningTimeout: null,
-  },
+	state: {
+		user: null,
+		roles: [],
+		databases: [],
+		currentDb: null,
+		email: null,
+		config: null,
+		runningTimeout: null,
+	},
 
-  getters: {
-    getUser (state) {
-      return state.user
-    },
-		getCurrendDb (state) {
+	getters: {
+		getUser(state) {
+			return state.user
+		},
+		getCurrendDb(state) {
 			return state.currentDb
 		},
-    getRoles (state) {
-      return state.roles
-    },
-    isServerAdmin (state) {
-      return state.roles.includes("_admin")
-    },
-    isAuthenticated (state) {
-      return state.user !== null
-    }
-  },
+		getRoles(state) {
+			return state.roles
+		},
+		isServerAdmin(state) {
+			return state.roles.includes("_admin")
+		},
+		isAuthenticated(state) {
+			return state.user !== null
+		}
+	},
 
-  mutations: {
-    authUser (state, userData) {
-      state.user = userData.user
-      state.roles = userData.roles
-			// move to product page
-			router.push('/product')
-    },
-    storeUser (state, user) {
-      state.user = user
-      // eslint-disable-next-line no-console
-      console.log("storeUser: user= " + state.user)
-    },
-    clearAuthData (state) {
-      state.user = null
-      state.roles = []
-      state.databases = []
-      state.currentDb = null
-      state.email = null
-      state.config = null
-      clearTimeout(state.runningTimeout)
-    },
-    setConfig (state, config) {
-      state.config = config
-    }
-  },
+	mutations: {
+		authUser(state, userData) {
+			state.user = userData.user
+			state.roles = userData.roles
+		},
+		storeUser(state, user) {
+			state.user = user
+			// eslint-disable-next-line no-console
+			console.log("storeUser: user= " + state.user)
+		},
+		clearAuthData(state) {
+			state.user = null
+			state.roles = []
+			state.databases = []
+			state.currentDb = null
+			state.email = null
+			state.config = null
+			clearTimeout(state.runningTimeout)
+		}
+	},
 
-  actions: {
-    refreshCookie ({dispatch, state}, payload) {
-      // eslint-disable-next-line no-console
-      console.log("refreshcookie: afterSeconds= " + payload.afterSeconds)
-      state.runningTimeout = setTimeout(() => {
-        globalAxios({
-          method: 'POST',
-          url: '/_session',
-          withCredentials: true,
-          data: {
-            name: payload.authData.name,
-            password: payload.authData.password
-          }
-        }).then (res => {
-          // eslint-disable-next-line no-console
-          console.log(res)
-          // eslint-disable-next-line no-console
-          console.log("recurse refreshCookie")
-          //Recurse
-          dispatch('refreshCookie', payload)
-        })
-        // eslint-disable-next-line no-console
-        .catch(error => console.log(error))
-      }, payload.afterSeconds * 1000)
-    },
+	actions: {
+		refreshCookie({
+			dispatch,
+			state
+		}, payload) {
+			// eslint-disable-next-line no-console
+			console.log("refreshcookie: afterSeconds= " + payload.afterSeconds)
+			state.runningTimeout = setTimeout(() => {
+				globalAxios({
+						method: 'POST',
+						url: '/_session',
+						withCredentials: true,
+						data: {
+							name: payload.authData.name,
+							password: payload.authData.password
+						}
+					}).then(res => {
+						// eslint-disable-next-line no-console
+						console.log(res)
+						// eslint-disable-next-line no-console
+						console.log("recurse refreshCookie")
+						//Recurse
+						dispatch('refreshCookie', payload)
+					})
+					// eslint-disable-next-line no-console
+					.catch(error => console.log(error))
+			}, payload.afterSeconds * 1000)
+		},
 
-    // Load the config file from this database
-    getConfig({state, commit}) {
-      globalAxios({
-        method: 'GET',
-        url: state.currentDb + '/config',
-        withCredentials: true,
-      }).then (res => {
-        if (res.status == 200) {
-          commit('setConfig', 'res.data')
-          // eslint-disable-next-line no-console
-          console.log('The configuration is loaded')
-        }
-      })
-      // eslint-disable-next-line no-console
-      .catch(error => console.log('Config doc missing in database ' + state.currentDb + '. Error = ' + error))
-    },
-
-    // Get the current DB name etc. for this user. Note that the user roles are already fetched
-    getOtherUserData({state, dispatch}) {
-      this.commit('clearAll')
-      globalAxios({
-        method: 'GET',
-        url: '_users/org.couchdb.user:' + state.user,
-        withCredentials: true
-      }).then(res => {
-        // eslint-disable-next-line no-console
-        console.log('getOtherUserData called for user = ' + state.user)
-        state.email = res.data.email
-        state.databases = res.data.databases
-        state.currentDb = res.data.currentDb
+		// Load the config file from this database
+		getConfig({
+			state,
+		}) {
+			globalAxios({
+					method: 'GET',
+					url: state.currentDb + '/config',
+					withCredentials: true,
+				}).then(res => {
+					if (res.status == 200) {
+						// eslint-disable-next-line no-console
+						console.log(res)
+						state.config = res.data
+						// eslint-disable-next-line no-console
+						console.log('The configuration is loaded, Move to product page')
+						// eslint-disable-next-line no-console
+						router.push('/product')
+					}
+				})
 				// eslint-disable-next-line no-console
-				console.log('getOtherUserData: database ' + state.currentDb + ' is set for user ' + state.user)
-        dispatch('getConfig')
-      })
-      .catch(error => {
-        // eslint-disable-next-line no-console
-        console.log('getOtherUserData error= ', error)
-      })
-    },
+				.catch(error => console.log('Config doc missing in database ' + state.currentDb + '. Error = ' + error))
+		},
 
-    login({commit, dispatch, state}, authData) {
-      globalAxios({
-        method: 'POST',
-        url: '/_session',
-        withCredentials: true,
-        data: {
-          name: authData.name,
-          password: authData.password
-        }
-      }).then (res => {
-        // eslint-disable-next-line no-console
-        console.log(res)
-        if (res.status == 200) {
-					state.user = res.data.name
-          commit('authUser', {
-            user: res.data.name,
-            roles: res.data.roles
-          })
-          dispatch('getOtherUserData')
-          //Refresh the session cookie after 6 minutes
-          dispatch('refreshCookie', {
-            authData,
-            loggedOut: state.loggedOut,
-            afterSeconds: 360
-          })}
-        })
-        // eslint-disable-next-line no-console
-        .catch(error => console.log(error))
-      },
+		// Get the current DB name etc. for this user. Note that the user roles are already fetched
+		getOtherUserData({
+			state,
+			dispatch
+		}) {
+			this.commit('clearAll')
+			globalAxios({
+					method: 'GET',
+					url: '_users/org.couchdb.user:' + state.user,
+					withCredentials: true
+				}).then(res => {
+					// eslint-disable-next-line no-console
+					console.log(res)
+					// eslint-disable-next-line no-console
+					console.log('getOtherUserData called for user = ' + state.user)
+					state.email = res.data.email
+					state.databases = res.data.databases
+					state.currentDb = res.data.currentDb
+					// eslint-disable-next-line no-console
+					console.log('getOtherUserData: database ' + state.currentDb + ' is set for user ' + state.user)
+					dispatch('getConfig')
+				})
+				.catch(error => {
+					// eslint-disable-next-line no-console
+					console.log('getOtherUserData error= ', error)
+				})
+		},
+
+		login({
+			commit,
+			dispatch,
+			state
+		}, authData) {
+			globalAxios({
+					method: 'POST',
+					url: '/_session',
+					withCredentials: true,
+					data: {
+						name: authData.name,
+						password: authData.password
+					}
+				}).then(res => {
+					// eslint-disable-next-line no-console
+					console.log(res)
+					if (res.status == 200) {
+						state.user = res.data.name
+						commit('authUser', {
+							user: res.data.name,
+							roles: res.data.roles
+						})
+						dispatch('getOtherUserData')
+						//Refresh the session cookie after 6 minutes
+						dispatch('refreshCookie', {
+							authData,
+							loggedOut: state.loggedOut,
+							afterSeconds: 360
+						})
+					}
+				})
+				// eslint-disable-next-line no-console
+				.catch(error => console.log(error))
+		},
 
 
-      logout ({commit}) {
-        commit('clearAuthData')
-        router.replace('/')
-      }
-    },
+		logout({
+			commit
+		}) {
+			commit('clearAuthData')
+			router.replace('/')
+		}
+	},
 
-    modules: {
-      setup
-    }
+	modules: {
+		setup
+	}
 
-  })
+})
