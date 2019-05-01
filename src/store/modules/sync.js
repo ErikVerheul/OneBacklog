dde03e89c784e321b488ebeaa8ba9b1f07a123e2
Erik Verheul <erik@verheulconsultants.nl>
2019-05-01 08:24:05 +0200

    More refactoring

--- src/store/modules/sync.js
+++ src/store/modules/sync.js
@@ -12,74 +12,6 @@ const actions = {
 		rootGetters,
 		dispatch
 	}, since) {
-		let url = rootState.currentDb + '/_changes?feed=longpoll&filter=_view&view=design1/changesFilter&include_docs=true'
-		if (since) url += '&since=' + since
-		else {
-			// initially get the last change only
-			url += '&descending=true&limit=1'
-		}
-		globalAxios({
-				method: 'GET',
-				url: url,
-				withCredentials: true
-			}).then(res => {
-				let data = res.data
-				rootState.lastSyncSeq = data.last_seq
-				let dateStr = new Date(Date.now())
-				//eslint-disable-next-line no-console
-				if (rootState.debug) console.log('listenForChanges: time = ' + dateStr)
-				let changedDocs = []
-				if (since && data.results.length > 0) {
-					for (let i = 0; i < data.results.length; i++) {
-						/*
-						 * Select only documents which are a product backlog item and
-						 * changes not made by the user him/her self and ment for distribution (if not filtered out by the _design filter)
-						 */
-						let doc = data.results[i].doc
-						if (doc.type === 'backlogItem' &&
-							doc.history[0].distributeEvent == true &&
-							doc.history[0].sessionId != rootState.sessionId &&
-							rootGetters.getUserAssignedProductIds.includes(doc.productId)) {
-							changedDocs.push(doc)
-						}
-					}
-					if (changedDocs.length > 0) {
-						const payload = {
-							changes: changedDocs,
-							next: 0
-						}
-						dispatch('processChangedDocs', payload)
-					}
-				}
-				rootState.listenForChangesRunning = true
-				// recurse
-				dispatch('listenForChanges', rootState.lastSyncSeq)
-			})
-			.catch(error => {
-				let msg = 'Listening for changes made by other users failed with ' + error
-				// eslint-disable-next-line no-console
-				console.log(msg)
-				if (rootState.currentDb) dispatch('doLog', {
-					event: msg,
-					level: "WARNING"
-				})
-				rootState.listenForChangesRunning = false
-			})
-	},
-
-	doBlinck({
-		state
-	}) {
-		state.eventSyncColor = '#e6f7ff'
-		setTimeout(function () {
-			state.eventSyncColor = '#004466'
-		}, 1000)
-	},
-
-	processChangedDocs({
-		rootState,
-		dispatch
-	}, payload) {
 		/*
 		 * Returns the node or null when it does not exist
 		 */
@@ -93,7 +25,6 @@ const actions = {
 			})
 			return resultNode
 		}
-
 		/*
 		 * When the node exists this function returns an object with:
 		 * - the previous node (can be the parent)
@@ -143,103 +74,145 @@ const actions = {
 				"data": newData
 			})
 		}
-
-		let doc = payload.changes[payload.next]
-		// eslint-disable-next-line no-console
-		if (rootState.debug) console.log('processChangedDocs: document with _id ' + doc._id + ' is processed.')
-		dispatch('doBlinck')
-		let node = getNodeById(doc._id)
-		if (node != null) {
-			// the node exists (is not new)
-			if (!doc.delmark) {
-				// update the parent as it can be changed
-				let locationInfo = getLocationInfo(node, doc.priority, doc.parentId)
-				// update priority, parent and product
-				node.data.priority = doc.priority
-				node.data.parentId = doc.parentId
-				node.data.productId = doc.productId
-				if (window.slVueTree.comparePaths(locationInfo.newPath, node.path) === 0) {
-					// the node has not changed parent nor changed location w/r to its siblings
-					updateFields(doc, node)
-				} else {
-					// move the node to the new position w/r to its siblings
-					if (window.slVueTree.comparePaths(locationInfo.newPath, node.path) === -1) {
-						// move up: remove from old position first
-						node.isLeaf = (locationInfo.newLevel < PBILEVEL) ? false : true
-						window.slVueTree.remove([node.path])
-						if (locationInfo.newInd === 0) {
-							window.slVueTree.insert({
-								node: locationInfo.prevNode,
-								placement: 'inside'
-							}, node)
-						} else {
-							// insert after prevNode
-							window.slVueTree.insert({
-								node: locationInfo.prevNode,
-								placement: 'after'
-							}, node)
-						}
-					} else {
-						// move down: insert first
-						node.isLeaf = (locationInfo.newLevel < PBILEVEL) ? false : true
-						if (locationInfo.newInd === 0) {
-							window.slVueTree.insert({
-								node: locationInfo.prevNode,
-								placement: 'inside'
-							}, node)
-						} else {
-							window.slVueTree.insert({
-								node: locationInfo.prevNode,
-								placement: 'after'
-							}, node)
-						}
-						// remove from old position
-						window.slVueTree.remove([node.path])
-					}
-				}
-			} else {
-				// remove the node
-				window.slVueTree.remove([node.path])
-			}
-		} else {
-			// new node
-			let node = {
-				"title": doc.title,
-				"isSelected": false,
-				"isExpanded": true,
-				"children": [],
-				"data": {
-					"_id": doc._id,
-					"productId": doc.productId,
-					"parentId": doc.parentId,
-					"subtype": 0,
-					"sessionId": rootState.sessionId,
-					"distributeEvent": true
-				}
-			}
-			let locationInfo = getLocationInfo(node, doc.priority, doc.parentId)
-			// update priority
-			node.data.priority = doc.priority
-			node.isLeaf = (locationInfo.newLevel < PBILEVEL) ? false : true
-			if (locationInfo.newInd === 0) {
-				window.slVueTree.insert({
-					node: locationInfo.prevNode,
-					placement: 'inside'
-				}, node)
-			} else {
-				window.slVueTree.insert({
-					node: locationInfo.prevNode,
-					placement: 'after'
-				}, node)
-			}
+		let url = rootState.currentDb + '/_changes?feed=longpoll&filter=_view&view=design1/changesFilter&include_docs=true'
+		if (since) url += '&since=' + since
+		else {
+			// initially get the last change only
+			url += '&descending=true&limit=1'
 		}
-		payload.next++
-		if (payload.next < payload.changes.length) {
-			// recurse
-			dispatch('processChangedDocs', payload)
-		}
-
+		globalAxios({
+				method: 'GET',
+				url: url,
+				withCredentials: true
+			}).then(res => {
+				let data = res.data
+				rootState.lastSyncSeq = data.last_seq
+				//eslint-disable-next-line no-console
+				if (rootState.debug) console.log('listenForChanges: time = ' + new Date(Date.now()))
+				if (since) {
+					for (let i = 0; i < data.results.length; i++) {
+						let doc = data.results[i].doc
+						// Select only documents which are a product backlog item and
+						// changes not made by the user him/her self and ment for distribution (if not filtered out by the _design filter)
+						if (doc.type === 'backlogItem' &&
+							doc.history[0].distributeEvent == true &&
+							doc.history[0].sessionId != rootState.sessionId &&
+							rootGetters.getUserAssignedProductIds.includes(doc.productId)) {
+							// eslint-disable-next-line no-console
+							if (rootState.debug) console.log('processChangedDocs: document with _id ' + doc._id + ' is processed.')
+							dispatch('doBlinck')
+							let node = getNodeById(doc._id)
+							if (node != null) {
+								// the node exists (is not new)
+								if (!doc.delmark) {
+									// update the parent as it can be changed
+									let locationInfo = getLocationInfo(node, doc.priority, doc.parentId)
+									// update priority, parent and product
+									node.data.priority = doc.priority
+									node.data.parentId = doc.parentId
+									node.data.productId = doc.productId
+									if (window.slVueTree.comparePaths(locationInfo.newPath, node.path) === 0) {
+										// the node has not changed parent nor changed location w/r to its siblings
+										updateFields(doc, node)
+									} else {
+										// move the node to the new position w/r to its siblings
+										if (window.slVueTree.comparePaths(locationInfo.newPath, node.path) === -1) {
+											// move up: remove from old position first
+											node.isLeaf = (locationInfo.newLevel < PBILEVEL) ? false : true
+											window.slVueTree.remove([node.path])
+											if (locationInfo.newInd === 0) {
+												window.slVueTree.insert({
+													node: locationInfo.prevNode,
+													placement: 'inside'
+												}, node)
+											} else {
+												// insert after prevNode
+												window.slVueTree.insert({
+													node: locationInfo.prevNode,
+													placement: 'after'
+												}, node)
+											}
+										} else {
+											// move down: insert first
+											node.isLeaf = (locationInfo.newLevel < PBILEVEL) ? false : true
+											if (locationInfo.newInd === 0) {
+												window.slVueTree.insert({
+													node: locationInfo.prevNode,
+													placement: 'inside'
+												}, node)
+											} else {
+												window.slVueTree.insert({
+													node: locationInfo.prevNode,
+													placement: 'after'
+												}, node)
+											}
+											// remove from old position
+											window.slVueTree.remove([node.path])
+										}
+									}
+								} else {
+									// remove the node
+									window.slVueTree.remove([node.path])
+								}
+							} else {
+								// new node
+								let node = {
+									"title": doc.title,
+									"isSelected": false,
+									"isExpanded": true,
+									"children": [],
+									"data": {
+										"_id": doc._id,
+										"productId": doc.productId,
+										"parentId": doc.parentId,
+										"subtype": 0,
+										"sessionId": rootState.sessionId,
+										"distributeEvent": true
+									}
+								}
+								let locationInfo = getLocationInfo(node, doc.priority, doc.parentId)
+								// update priority
+								node.data.priority = doc.priority
+								node.isLeaf = (locationInfo.newLevel < PBILEVEL) ? false : true
+								if (locationInfo.newInd === 0) {
+									window.slVueTree.insert({
+										node: locationInfo.prevNode,
+										placement: 'inside'
+									}, node)
+								} else {
+									window.slVueTree.insert({
+										node: locationInfo.prevNode,
+										placement: 'after'
+									}, node)
+								}
+							}
+						}
+					} // end of loop
+				}
+				rootState.listenForChangesRunning = true
+				// recurse
+				dispatch('listenForChanges', rootState.lastSyncSeq)
+			})
+			.catch(error => {
+				let msg = 'Listening for changes made by other users failed with ' + error
+				// eslint-disable-next-line no-console
+				console.log(msg)
+				if (rootState.currentDb) dispatch('doLog', {
+					event: msg,
+					level: "WARNING"
+				})
+				rootState.listenForChangesRunning = false
+			})
 	},
+
+	doBlinck({
+		state
+	}) {
+		state.eventSyncColor = '#e6f7ff'
+		setTimeout(function () {
+			state.eventSyncColor = '#004466'
+		}, 1000)
+	}
 }
 
 export default {
