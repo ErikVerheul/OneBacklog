8f3c1bce03a2a0f50b6a3b528119292b2ca36560
Erik Verheul <erik@verheulconsultants.nl>
2019-05-21 14:30:58 +0200

    Sidebar removed; v-if relocated

--- src/components/sl-vue-tree/sl-vue-tree.vue
+++ src/components/sl-vue-tree/sl-vue-tree.vue
@@ -2,18 +2,17 @@
 <template>
 	<div class="sl-vue-tree" :class="{'sl-vue-tree-root': isRoot }" @mousemove="onMousemoveHandler" @mouseleave="onMouseleaveHandler" @dragend="onDragendHandler(null, $event)">
 		<div ref="nodes" class="sl-vue-tree-nodes-list">
-			<div class="sl-vue-tree-node" v-for="(node, nodeInd) in nodes" :class="{'sl-vue-tree-selected': node.isSelected}" :key="node.pathStr">
-				<div v-if="node.doShow">
-					<div class="sl-vue-tree-cursor sl-vue-tree-cursor_before" @dragover.prevent :style="{
+			<div v-if="node.doShow" class="sl-vue-tree-node" v-for="(node, nodeInd) in nodes" :class="{'sl-vue-tree-selected': node.isSelected}" :key="node.pathStr">
+				<div class="sl-vue-tree-cursor sl-vue-tree-cursor_before" @dragover.prevent :style="{
 						visibility:
             cursorPosition &&
             cursorPosition.node.pathStr === node.pathStr &&
             cursorPosition.placement === 'before' ? 'visible' : 'hidden'
            }">
-						<!-- suggested place for node insertion  -->
-					</div>
+					<!-- suggested place for node insertion  -->
+				</div>
 
-					<div class="sl-vue-tree-node-item" @mousedown="onNodeMousedownHandler($event, node)" @mouseup="onNodeMouseupHandler($event, node)" @contextmenu="emitNodeContextmenu(node, $event)" @dblclick="emitNodeDblclick(node, $event)" @click="emitNodeClick(node, $event)" @dragover="onExternalDragoverHandler(node, $event)" @drop="onExternalDropHandler(node, $event)" :path="node.pathStr" :class="{
+				<div class="sl-vue-tree-node-item" @mousedown="onNodeMousedownHandler($event, node)" @mouseup="onNodeMouseupHandler($event, node)" @contextmenu="emitNodeContextmenu(node, $event)" @dblclick="emitNodeDblclick(node, $event)" @click="emitNodeClick(node, $event)" @dragover="onExternalDragoverHandler(node, $event)" @drop="onExternalDropHandler(node, $event)" :path="node.pathStr" :class="{
             'sl-vue-tree-cursor-hover':
               cursorPosition &&
               cursorPosition.node.pathStr === node.pathStr,
@@ -25,73 +24,67 @@
             'sl-vue-tree-node-is-leaf' : node.isLeaf,
             'sl-vue-tree-node-is-folder' : !node.isLeaf
           }">
-						<div class="sl-vue-tree-gap" v-for="gapInd in gaps" :key="gapInd"></div>
+					<div class="sl-vue-tree-gap" v-for="gapInd in gaps" :key="gapInd"></div>
 
-						<div class="sl-vue-tree-branch" v-if="level && showBranches">
-							<slot name="branch" :node="node">
-								<span v-if="!node.isLastChild">
-									{{ String.fromCharCode(0x251C) }}{{ String.fromCharCode(0x2500) }}&nbsp;
-								</span>
-								<span v-if="node.isLastChild">
-									{{String.fromCharCode(0x2514) }}{{ String.fromCharCode(0x2500) }}&nbsp;
-								</span>
-							</slot>
-						</div>
-
-						<div class="sl-vue-tree-title">
-							<span class="sl-vue-tree-toggle" v-if="!node.isLeaf" @click="onToggleHandler($event, node)">
-								<slot name="toggle" :node="node">
-									<span>
-										{{ !node.isLeaf ? (node.isExpanded ? '-' : '+') : '' }}
-									</span>
-								</slot>
+					<div class="sl-vue-tree-branch" v-if="level && showBranches">
+						<slot name="branch" :node="node">
+							<span v-if="!node.isLastChild">
+								{{ String.fromCharCode(0x251C) }}{{ String.fromCharCode(0x2500) }}&nbsp;
 							</span>
-
-							<slot name="title" :node="node">{{ node.title }}</slot>
-
-							<slot name="empty-node" :node="node" v-if="!node.isLeaf && node.children.length == 0 && node.isExpanded">
-							</slot>
-
-						</div>
-
-						<div class="sl-vue-tree-sidebar">
-							<slot name="sidebar" :node="node"></slot>
-						</div>
-
+							<span v-if="node.isLastChild">
+								{{String.fromCharCode(0x2514) }}{{ String.fromCharCode(0x2500) }}&nbsp;
+							</span>
+						</slot>
 					</div>
 
-					<sl-vue-tree v-if="node.children && node.children.length && node.isExpanded" :value="node.children" :level="node.level" :parentInd="nodeInd" :allowMultiselect="allowMultiselect" :allowToggleBranch="allowToggleBranch" :edgeSize="edgeSize" :showBranches="showBranches" @dragover.prevent>
-						<template slot="title" slot-scope="{ node }">
-							<slot name="title" :node="node">{{ node.title }}</slot>
-						</template>
-
-						<template slot="toggle" slot-scope="{ node }">
+					<div class="sl-vue-tree-title">
+						<span class="sl-vue-tree-toggle" v-if="!node.isLeaf" @click="onToggleHandler($event, node)">
 							<slot name="toggle" :node="node">
 								<span>
 									{{ !node.isLeaf ? (node.isExpanded ? '-' : '+') : '' }}
 								</span>
 							</slot>
-						</template>
+						</span>
 
-						<template slot="sidebar" slot-scope="{ node }">
-							<slot name="sidebar" :node="node"></slot>
-						</template>
+						<slot name="title" :node="node">{{ node.title }}</slot>
 
-						<template slot="empty-node" slot-scope="{ node }">
-							<slot name="empty-node" :node="node" v-if="!node.isLeaf && node.children.length == 0 && node.isExpanded">
-							</slot>
-						</template>
+						<slot name="empty-node" :node="node" v-if="!node.isLeaf && node.children.length == 0 && node.isExpanded">
+						</slot>
 
-					</sl-vue-tree>
+					</div>
+				</div>
 
-					<div class="sl-vue-tree-cursor sl-vue-tree-cursor_after" @dragover.prevent :style="{
+				<sl-vue-tree v-if="node.children && node.children.length && node.isExpanded" :value="node.children" :level="node.level" :parentInd="nodeInd" :allowMultiselect="allowMultiselect" :allowToggleBranch="allowToggleBranch" :edgeSize="edgeSize" :showBranches="showBranches" @dragover.prevent>
+					<template slot="title" slot-scope="{ node }">
+						<slot name="title" :node="node">{{ node.title }}</slot>
+					</template>
+
+					<template slot="toggle" slot-scope="{ node }">
+						<slot name="toggle" :node="node">
+							<span>
+								{{ !node.isLeaf ? (node.isExpanded ? '-' : '+') : '' }}
+							</span>
+						</slot>
+					</template>
+
+					<template slot="sidebar" slot-scope="{ node }">
+						<slot name="sidebar" :node="node"></slot>
+					</template>
+
+					<template slot="empty-node" slot-scope="{ node }">
+						<slot name="empty-node" :node="node" v-if="!node.isLeaf && node.children.length == 0 && node.isExpanded">
+						</slot>
+					</template>
+
+				</sl-vue-tree>
+
+				<div class="sl-vue-tree-cursor sl-vue-tree-cursor_after" @dragover.prevent :style="{
               visibility:
                cursorPosition && isDragging &&
                cursorPosition.node.pathStr === node.pathStr &&
                cursorPosition.placement === 'after' ? 'visible' : 'hidden'
              }">
-						<!-- suggested place for node insertion  -->
-					</div>
+					<!-- suggested place for node insertion  -->
 				</div>
 			</div>
 
