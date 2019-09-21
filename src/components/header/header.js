3b2c9c0352bcf010d69292a14976af880ecf4e3a
Erik Verheul <erik@verheulconsultants.nl>
2019-09-21 16:01:43 +0200

    Version update

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ export default {
     mixins: [utilities],
     data() {
         return {
-            appVersion: "OneBackLog v.0.6.2",
+            appVersion: "OneBackLog v.0.6.2.1",
             oldPassword: "",
             newPassword1: "",
             newPassword2: "",
