f6832535f1a64b0f0e0cd3e1647b632177fc5f31
Erik Verheul <erik@verheulconsultants.nl>
2019-11-15 20:05:49 +0100

    Bump the application version number

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ export default {
     mixins: [utilities],
     data() {
         return {
-            appVersion: "OneBackLog v.0.7.0",
+            appVersion: "OneBackLog v.0.7.1",
             oldPassword: "",
             newPassword1: "",
             newPassword2: "",
