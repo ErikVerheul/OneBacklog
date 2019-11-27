fce922d65e3595f1380f256a3645a08fee27ce4d
Erik Verheul <erik@verheulconsultants.nl>
2019-11-27 10:10:01 +0100

    No need for this import

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import store from '../store/store'
