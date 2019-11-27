16be488294b415f9bbcad8e6f7667d4572c055c9
Erik Verheul <erik@verheulconsultants.nl>
2019-11-27 10:29:23 +0100

    Undo removal - this css is needed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import store from '../store/store'
