5b7443a1824226aa83e1c5a68b8d363cb451fbab
Erik Verheul <erik@verheulconsultants.nl>
2019-12-31 17:58:31 +0100

    Run this application on a desktop computer or tablet

--- src/components/welcome/welcome.vue
+++ src/components/welcome/welcome.vue
@@ -10,7 +10,7 @@
       <div class="col-lg-12 col-sm-12 text-center">
         <h3>Welcome to the OneBacklog single page web application</h3>
         <p>This application is intended for Product Owners who own one ore more products as well as for developers and viewers.<br>
-           Your role determines what you can do. The application is in beta.</p>
+           Your role determines what you can do. The application is in beta. Run this application on a desktop computer or tablet.</p>
 				<p>Signin with <b>demoUser</b> and password <b>demoUser</b>.</p>
       </div>
     </div>
