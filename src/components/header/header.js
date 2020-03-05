85cffee82e66137d904e49cc72b23035c65e0fd8
Erik Verheul <erik@verheulconsultants.nl>
2020-03-05 11:44:43 +0100

    Bump version number

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ export default {
     mixins: [utilities],
     data() {
         return {
-            appVersion: "OneBackLog v.0.9.0",
+            appVersion: "OneBackLog v.0.9.1",
             oldPassword: "",
             newPassword1: "",
             newPassword2: "",
