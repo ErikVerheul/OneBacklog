212e6f18c909fe25f25165f18299f2368f83419f
Erik Verheul <erik@verheulconsultants.nl>
2020-03-29 17:42:24 +0200

    Create more height for color selection

--- src/components/views/coarse_product/c_product_view.vue
+++ src/components/views/coarse_product/c_product_view.vue
@@ -194,7 +194,7 @@
               </div>
             </div>
           </div>
-          <div class="pane" :style="{ minHeight: '40px', height: '40px', maxHeight: '40px' }">
+          <div class="pane" :style="{ minHeight: '40px', height: '60px', maxHeight: '60px' }">
             <div class="d-table w-100">
               <template v-if="!isReqAreaItem" >
                 <p v-if="getCurrentItemLevel < taskLevel" class="title is-6">This {{ getLevelText(getCurrentItemLevel) }} is owned by team '{{ $store.state.currentDoc.team }}'</p>
