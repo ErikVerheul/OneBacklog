abacb05bfe484c453bfad1cdce59436dc2992093
Erik Verheul <erik@verheulconsultants.nl>
2020-04-03 18:57:16 +0200

    Sprint selection

--- src/components/views/planning_board/PlanningBoard.vue
+++ src/components/views/planning_board/PlanningBoard.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="board">
-    <app-header></app-header>
+    <app-header>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+          <b-nav-text>
+            Start: {{ getStartDateString }}
+          </b-nav-text>
+          <div class="divider"/>
+      </b-navbar-nav>
+
+      <b-navbar-nav>
+        <b-nav-form>
+          <b-form-select v-model="selectedSprint" :options="options"></b-form-select>
+        </b-nav-form>
+      </b-navbar-nav>
+
+      <b-navbar-nav>
+        <div class="divider"/>
+        <b-nav-text>
+          End: {{ getEndDateString }}
+        </b-nav-text>
+      </b-navbar-nav>
+    </app-header>
+
     <b-container fluid>
       <div v-for="item in $store.state.stories" :key="item.id">
         <b-row>
@@ -16,11 +38,73 @@
 <script>
 import appHeader from '../../header/header.vue'
 import StoryLane from './StoryLane'
+import { utilities } from '../../mixins/utilities.js'
 
 export default {
-  beforeCreate() {
-    this.$store.dispatch('loadCurrentSprint')
+  mixins: [utilities],
+
+  created() {
+    // load the current sprint before any sprint is selected
+    const now = Date.now()
+    let currentSprint = undefined
+    for (let i = 0; i < this.$store.state.configData.defaultSprintCalendar.length; i++) {
+      const s = this.$store.state.configData.defaultSprintCalendar[i]
+      if (s.startTimestamp < now && now < s.startTimestamp + s.sprintLength) {
+        currentSprint = s
+        break
+      }
+    }
+    // preset to the current sprint
+    this.selectedSprint = currentSprint
+    this.$store.dispatch('loadPlanningBoard', currentSprint)
   },
+
+  mounted() {
+    // create the sprint selection options, recent first + next sprint on top
+    const now = Date.now()
+    let getNextSprint = true
+    for (let i = this.$store.state.configData.defaultSprintCalendar.length - 1; i >= 0; i--) {
+      const sprint = this.$store.state.configData.defaultSprintCalendar[i]
+      if (sprint.startTimestamp > now) continue
+
+      if (getNextSprint) {
+        const nextSprint = this.$store.state.configData.defaultSprintCalendar[i + 1]
+        this.options.push({ value: nextSprint, text: nextSprint.name + ' (next sprint)' })
+        getNextSprint = false
+      }
+      this.options.push({ value: sprint, text: sprint.name })
+    }
+  },
+
+  data() {
+    return {
+      selectedSprint: null,
+      options: []
+    }
+  },
+
+  watch: {
+    selectedSprint: function(newVal) {
+      this.$store.dispatch('loadPlanningBoard', newVal)
+    }
+  },
+
+  computed: {
+    getStartDateString() {
+      if (this.selectedSprint) return new Date(this.selectedSprint.startTimestamp).toString().substring(0, 33)
+      return ''
+    },
+    getEndDateString() {
+      if (this.selectedSprint) return new Date(this.selectedSprint.startTimestamp + this.selectedSprint.sprintLength).toString().substring(0, 33)
+      return ''
+    }
+  },
+
+  methods: {
+
+
+  },
+
 
   name: 'PlanningBoard',
   components: {
@@ -29,3 +113,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.divider{
+    width:15px;
+    height:auto;
+    display:inline-block;
+}
+</style>
+
