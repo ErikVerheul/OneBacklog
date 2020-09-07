6fb2316ac7567e5a3e19c8cd5566c54b72dfaf3b
Erik Verheul <erik@verheulconsultants.nl>
2020-09-07 20:41:20 +0200

    Changes to the setting for the Webpack webserver needed for developing with https

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+//  publicPath: '/',
+
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+
+    https: true,
+    key: fs.readFileSync('C:/Users/erik.MYNETWORK/.ssl/localhost.key'),
+    cert: fs.readFileSync('C:/Users/erik.MYNETWORK/.ssl/localhost.crt'),
+    ca: fs.readFileSync('C:/Users/erik.MYNETWORK/.ssl/RootCA.pem')
+  }
 }
