d88f5721b4ff459427be40268a34fd5ded40750e
Erik Verheul <erik@verheulconsultants.nl>
2020-10-07 20:13:40 +0200

    Configure dev server in development mode only

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+const fs = require('fs')
 // eslint-disable-next-line no-console
-console.log('process.env.VUE_APP_SSL_PATH = ' + process.env.VUE_APP_SSL_PATH)
+console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV)
+// eslint-disable-next-line no-console
+if (process.env.NODE_ENV === 'development') console.log('process.env.VUE_APP_SSL_PATH = ' + process.env.VUE_APP_SSL_PATH)
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/var/www/html' : '/',
-  devServer: {
+  devServer: process.env.NODE_ENV === 'development' ? {
     host: 'localhost',
     port: 8080,
-
     https: true,
     key: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.key'),
     cert: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.crt'),
     ca: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/RootCA.pem')
-  }
+  } : {}
 }
