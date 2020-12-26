2fd283d7504544775fc5316d26101fe4115fe2f8
Erik Verheul <erik@verheulconsultants.nl>
2020-12-26 09:23:40 +0100

    Use v-else

--- src/components/initdb/initdb.vue
+++ src/components/initdb/initdb.vue
@@ -34,8 +34,8 @@
           <b-button v-if="dbName" class="m-1" @click="doCreateDatabase">Create database</b-button>
           <b-button class="m-1" @click="cancel" variant="outline-primary">Cancel</b-button>
       </div>
-      <div v-if="$store.state.isDatabaseInitiated">
-        <h5>Succes! Apart from being a CouchDb 'server admin' you have the 'admin' role.</h5>
+      <div v-else>
+        <h5>Success! Apart from being a CouchDb 'server admin' you have the 'admin' role.</h5>
         <h5>Exit and sign-in again. The 'Admin' view will open. Create a default sprint calendar and create the first users and set their roles. Assign one or more admins to take over your admin task.</h5>
         <b-button class="m-1" @click="signIn" variant="outline-primary">Exit</b-button>
       </div>
