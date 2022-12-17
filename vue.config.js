3d7ffeed07133189ed79578acffe0810306c79c5
Erik Verheul <erik@verheulconsultants.nl>
2022-12-17 20:17:36 +0100

    Migrate to Webpack v4+, See https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md

--- vue.config.js
+++ vue.config.js
@@ -9,9 +9,10 @@ module.exports = {
   devServer: process.env.NODE_ENV === 'development' ? {
     host: 'localhost',
     port: 8080,
-    https: true,
-    key: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.key'),
-    cert: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.crt'),
+    https: {
+      key: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.key'),
+      cert: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.crt')
+  },
   } : {},
 
   productionSourceMap: false
