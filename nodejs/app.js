83b596a82c587951b2ece0deaa5132c6e5b26dd4
Erik Verheul <erik@verheulconsultants.nl>
2023-08-31 10:17:20 +0200

    Allow unicode characters when decoding base64 in emails send

--- nodejs/app.js
+++ nodejs/app.js
@@ -13,6 +13,32 @@ var db
 var configData = {}
 var runData = {}
 
+/* The "Unicode Problem" See https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem
+Since btoa interprets the code points of its input string as byte values, 
+calling btoa on a string will cause a "Character Out Of Range" exception if a character's code point exceeds 0xff. 
+For use cases where you need to encode arbitrary Unicode text, 
+it is necessary to first convert the string to its constituent bytes in UTF-8, and then encode the bytes.*/
+
+function base64ToBytes(base64) {
+	const binString = atob(base64)
+	return Uint8Array.from(binString, (m) => m.codePointAt(0))
+}
+
+function bytesToBase64(bytes) {
+	const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join("")
+	return window.btoa(binString)
+}
+
+// convert unicode string to base64 encoded ascii
+function utoa(str) {
+    return bytesToBase64(new TextEncoder().encode(str))
+}
+
+// convert base64 encoded ascii to unicode string
+function atou(bytes) {
+    return new TextDecoder().decode(base64ToBytes(bytes))
+}
+
 function getSubTypeText(dbName, idx) {
 	if (idx < 0 || idx >= configData[dbName].subtype.length) {
 		return 'Error: unknown subtype'
@@ -65,9 +91,9 @@ function mkHtml(dbName, eventType, value, event, doc) {
 	}
 	switch (eventType) {
 		case "acceptanceEvent":
-			return mkHeader() + `<h3>The acceptance criteria changed from:</h3><p>${atob(value[0])}</p> to <p>${atob(value[1])}</p>` + mkFooter()
+			return mkHeader() + `<h3>The acceptance criteria changed from:</h3><p>${atou(value[0])}</p> to <p>${atou(value[1])}</p>` + mkFooter()
 		case "addCommentEvent":
-			return mkHeader() + `<h3>The user added a comment:</h3><p>${atob(value[0])}</p>` + mkFooter()
+			return mkHeader() + `<h3>The user added a comment:</h3><p>${atou(value[0])}</p>` + mkFooter()
 		case "addSprintIdsEvent":
 			{
 				let txt = `This ${getLevelText(dbName, value[0], value[1])} is assigned to sprint '${value[2]}'.`
@@ -77,7 +103,7 @@ function mkHtml(dbName, eventType, value, event, doc) {
 		case "cloneEvent":
 			return mkHeader() + `<h3>This ${getLevelText(dbName, value[0], value[1])} has been cloned as item of product '${value[2]}'.</h3>` + mkFooter()
 		case "commentToHistoryEvent":
-			return mkHeader() + `<h3>The user added comment:</h3><p>${atob(value[0])}</p><h3>to the history of this item</h3>` + mkFooter()
+			return mkHeader() + `<h3>The user added comment:</h3><p>${atou(value[0])}</p><h3>to the history of this item</h3>` + mkFooter()
 		case "conditionRemovedEvent":
 			{
 				let s
@@ -95,7 +121,7 @@ function mkHtml(dbName, eventType, value, event, doc) {
 				return mkHeader() + `<h3>${s}</h3>` + + mkFooter()
 			}
 		case "descriptionEvent":
-			return mkHeader() + `<h3>The description changed from:</h3><p>${atob(value[0])}</p> to <p>${atob(value[1])}</p>` + mkFooter()
+			return mkHeader() + `<h3>The description changed from:</h3><p>${atou(value[0])}</p> to <p>${atou(value[1])}</p>` + mkFooter()
 		case "undoBranchRemovalEvent":
 			return mkHeader() + `<h3>The ${this.getLevelText(value[9], value[10])} with title '${value[11]}' and ${value[1]} descendants are restored from removal.</h3>` + mkFooter()
 		case "newChildEvent":
