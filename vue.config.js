256567596f39b60994706068713905d99e554454
Erik Verheul <erik@verheulconsultants.nl>
2023-09-02 10:30:45 +0200

    Remove uncommented code

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,7 @@ module.exports = {
 
   devServer: process.env.NODE_ENV === 'development' ? {
     host: 'localhost',
-    port: 8080,
-    // https: {
-    //   key: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.key'),
-    //   cert: fs.readFileSync(process.env.VUE_APP_SSL_PATH + '/localhost.crt')
-    // },
+    port: 8080
   } : {},
 
   productionSourceMap: false
