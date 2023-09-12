b60b19cf1e4585300ef13d963f69f9f7b79014dd
Erik Verheul <erik@verheulconsultants.nl>
2023-09-12 20:53:06 +0200

    No need for the webpack webSocketServer

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ module.exports = {
   lintOnSave: true,
 
   devServer: process.env.NODE_ENV === 'development' ? {
+    // no need for the webpack webSocketServer
+    webSocketServer: false,
     host: 'localhost',
     port: 8080
   } : {},
