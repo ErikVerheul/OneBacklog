d113761e559620b852804a5779b4ee9c3260319c
Erik Verheul <erik@verheulconsultants.nl>
2024-02-19 17:26:45 +0100

    Update script to Vue3

--- scripts/removeOldUpload.sh
+++ scripts/removeOldUpload.sh
@@ -5,8 +5,7 @@ rm index.html
 echo "place maintenance.html"
 cp maintenance.html $WEB_DIR/index.html
 rm favicon.ico
-rm -r css
+rm -r assets
 rm -r img
-rm -r js
 ls -al
 echo "done removing old upload and replacing index.html with maintenance.html"
