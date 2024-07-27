17da30bf0004dcfad914b22c2ea34615813afc96
Erik Verheul <erik@verheulconsultants.nl>
2024-07-27 13:31:08 +0200

    Epics have no owning team. Features, user stories and tasks have

--- src/components/views/detail_product/d_product_view.js
+++ src/components/views/detail_product/d_product_view.js
@@ -66,7 +66,7 @@ const methods = {
 
 	getItemInfo() {
 		let txt = ''
-		if (this.getCurrentItemLevel > LEVEL.PRODUCT) {
+		if (this.getCurrentItemLevel > LEVEL.EPIC) {
 			if (this.getCurrentItemLevel < LEVEL.TASK) {
 				txt = `This ${this.getLevelText(this.getCurrentItemLevel)} is owned by team '${store.state.currentDoc.team}'`
 			} else {
