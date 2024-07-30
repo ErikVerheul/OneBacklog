b2492477f71b8200ed46bdf16d24b475af556cec
Erik Verheul <erik@verheulconsultants.nl>
2024-07-30 11:54:11 +0200

    Add now CouchDb design view

--- src/store/modules/initdb.js
+++ src/store/modules/initdb.js
@@ -346,6 +346,13 @@ const actions = {
 			data: {
 				views: {
 					/* Filter on items with assigned requirement area */
+					assignedTasksToUser: {
+						map: `function(doc) {
+							const TASKLEVEL = 6
+							if (doc.type == "backlogItem" && !doc.delmark && doc.level === TASKLEVEL && doc.taskOwner) emit([doc.team, doc.taskOwner, doc.productId, doc.sprintId], 1)
+						}`
+					},
+					/* Filter on items with assigned requirement area */
 					assignedToReqArea: {
 						map: `function(doc) {
 							if (doc.type == "backlogItem" && !doc.delmark && doc.reqarea) emit(doc.reqarea, 1)
