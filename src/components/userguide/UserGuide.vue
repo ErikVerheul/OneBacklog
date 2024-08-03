43462de269f12cc8274cdb942e0219461f792561
Erik Verheul <erik@verheulconsultants.nl>
2024-08-03 19:52:50 +0200

    Correct  to current implementation

--- src/components/userguide/UserGuide.vue
+++ src/components/userguide/UserGuide.vue
@@ -236,7 +236,7 @@
 							<h5>Select an item on (short) Id</h5>
 							<p>You can also use the full, 18+ positions long, id. More convenient is the use of the short Id. This Id is
 								5 characters long. It is displayed in the product view right from the item title. When you enter an Id and press
-								Enter or click outside this field:</p>
+								Enter:</p>
 							<ul>
 								<li>The item is found in the currently selected and opened or expanded product. The item will be selected and
 									highlighted in the tree view.</li>
@@ -253,7 +253,7 @@
 							<p>To return to the original view, click on the white X next to the input field or return to the tree state
 								before a filter was set by clicking the 'Clear filter and ...' button.</p>
 							<h5>Search in titles</h5>
-							<p>After entering a text in this input field, press Enter or click outside this field. Any item of any selected product
+							<p>After entering a text in this input field, press Enter. Any item of any selected product
 								wich has a (partial) match with the text in the title will be expanded and highlighted.
 								The first match will be selected. To return to the original view, click on the white X next to the input field.</p>
 						</div>
