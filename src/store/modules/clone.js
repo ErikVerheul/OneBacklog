1cceb61d5b8264112aa6c0275d3da1655c20ceab
Erik Verheul <erik@verheulconsultants.nl>
2024-08-14 10:45:54 +0200

    User Prettier formater on save

--- src/store/modules/clone.js
+++ src/store/modules/clone.js
@@ -32,20 +32,16 @@ function calcNewClonePriority(rootState, node) {
 
 const actions = {
 	/*
-	* Order of execution:
-	* 1. cloneBranch, dispatches processItemsToClone for the root document of the items to be cloned (the selected node)
-	* 2. processItemsToClone, dispatches getChildrenToClone for every document in the passed results array
-	* 3. getChildrenToClone, dispatches processItemsToClone for every parent id and dispatches addHistToClonedDoc when all parent ids are processed
-	* 4. addHistToClonedDoc, adds history to the cloned item, updates the tree view and creates undo data
-	* Attachments, dependencies, conditions, sprintId, color (reqarea items), delmark, unremovedMark and history are not copied.
-	* The event is not distributed to other on-line users.
-	* Only one clone action can be executed at a time.
-	*/
-	cloneBranch({
-		rootState,
-		dispatch,
-		commit
-	}, originalNode) {
+	 * Order of execution:
+	 * 1. cloneBranch, dispatches processItemsToClone for the root document of the items to be cloned (the selected node)
+	 * 2. processItemsToClone, dispatches getChildrenToClone for every document in the passed results array
+	 * 3. getChildrenToClone, dispatches processItemsToClone for every parent id and dispatches addHistToClonedDoc when all parent ids are processed
+	 * 4. addHistToClonedDoc, adds history to the cloned item, updates the tree view and creates undo data
+	 * Attachments, dependencies, conditions, sprintId, color (reqarea items), delmark, unremovedMark and history are not copied.
+	 * The event is not distributed to other on-line users.
+	 * Only one clone action can be executed at a time.
+	 */
+	cloneBranch({ rootState, dispatch, commit }, originalNode) {
 		if (busyCloning) {
 			commit('addToEventList', { txt: `Cannot start a clone while another clone is busy. Please try later`, severity: SEV.WARNING })
 			return
@@ -61,23 +57,28 @@ const actions = {
 		// get the document
 		globalAxios({
 			method: 'GET',
-			url: rootState.userData.currentDb + '/' + originalNode._id
-		}).then(res => {
-			const doc = res.data
-			// pass the cloneParentNode as undefined; the root node will be inserted in the tree view after all descendants are attached
-			dispatch('processItemsToClone', { originalNode, cloneProductId, results: [doc], newParentId: doc.parentId, cloneParentId: doc.parentId, cloneParentNode: undefined })
-		}).catch(error => {
-			const msg = `cloneBranch: Could not read the document with id ${originalNode._id} from database ${rootState.userData.currentDb}, ${error}`
-			dispatch('doLog', { event: msg, level: SEV.ERROR })
-			busyCloning = false
+			url: rootState.userData.currentDb + '/' + originalNode._id,
 		})
+			.then((res) => {
+				const doc = res.data
+				// pass the cloneParentNode as undefined; the root node will be inserted in the tree view after all descendants are attached
+				dispatch('processItemsToClone', {
+					originalNode,
+					cloneProductId,
+					results: [doc],
+					newParentId: doc.parentId,
+					cloneParentId: doc.parentId,
+					cloneParentNode: undefined,
+				})
+			})
+			.catch((error) => {
+				const msg = `cloneBranch: Could not read the document with id ${originalNode._id} from database ${rootState.userData.currentDb}, ${error}`
+				dispatch('doLog', { event: msg, level: SEV.ERROR })
+				busyCloning = false
+			})
 	},
 
-	processItemsToClone({
-		rootState,
-		dispatch,
-		commit
-	}, payload) {
+	processItemsToClone({ rootState, dispatch, commit }, payload) {
 		const toDispatch = []
 		const clonedDocs = []
 		for (const doc of payload.results) {
@@ -101,10 +102,12 @@ const actions = {
 				acceptanceCriteria: doc.acceptanceCriteria,
 				priority: doc._id === payload.originalNode._id ? calcNewClonePriority(rootState, payload.originalNode) : doc.priority,
 				comments: doc.comments,
-				history: [{
-					ignoreEvent: ['processItemsToClone'],
-					timestamp: Date.now()
-				}],
+				history: [
+					{
+						ignoreEvent: ['processItemsToClone'],
+						timestamp: Date.now(),
+					},
+				],
 				delmark: false,
 				// add last changes
 				lastPositionChange: doc.lastPositionChange,
@@ -125,7 +128,7 @@ const actions = {
 			// add history to the created clone
 			const newHist = {
 				ignoreEvent: ['cloneDescendants'],
-				timestamp: Date.now()
+				timestamp: Date.now(),
 			}
 			clonedDoc.history.unshift(newHist)
 			clonedDocs.push(clonedDoc)
@@ -134,105 +137,128 @@ const actions = {
 			if (payload.cloneParentNode) payload.cloneParentNode.children.push(clonedNode)
 			// multiple instances can be dispatched
 			toDispatch.push({
-				getChildrenToClone: { originalNode: payload.originalNode, cloneProductId: payload.cloneProductId, parentId: doc._id, cloneParentId: clonedDoc._id, cloneParentNode: clonedNode }
+				getChildrenToClone: {
+					originalNode: payload.originalNode,
+					cloneProductId: payload.cloneProductId,
+					parentId: doc._id,
+					cloneParentId: clonedDoc._id,
+					cloneParentNode: clonedNode,
+				},
 			})
 		}
 
 		dispatch('updateBulk', {
-			dbName: rootState.userData.currentDb, docs: clonedDocs, toDispatch, caller: 'processItemsToClone', onSuccessCallback: () => {
+			dbName: rootState.userData.currentDb,
+			docs: clonedDocs,
+			toDispatch,
+			caller: 'processItemsToClone',
+			onSuccessCallback: () => {
 				clonedDocsCount += clonedDocs.length
 				commit('startOrContinueShowProgress', `${clonedDocsCount - 1} descendants are cloned`)
-			}
+			},
 		})
 	},
 
-	getChildrenToClone({
-		rootState,
-		dispatch
-	}, payload) {
+	getChildrenToClone({ rootState, dispatch }, payload) {
 		globalAxios({
 			method: 'GET',
-			url: rootState.userData.currentDb + '/_design/design1/_view/docToParentMap?' + composeRangeString2(payload.parentId) + '&include_docs=true'
-		}).then(res => {
-			runningThreadsCount--
-			const results = res.data.rows
-			if (results.length > 0) {
-				// process next level
-				dispatch('processItemsToClone',
-					{ originalNode: payload.originalNode, cloneProductId: payload.cloneProductId, results: results.map(r => r.doc), newParentId: payload.parentId, cloneParentId: payload.cloneParentId, cloneParentNode: payload.cloneParentNode })
-			} else {
-				if (runningThreadsCount === 0) {
-					// db iteration ready
-					dispatch('addHistToClonedDoc', payload)
-				}
-			}
-		}).catch(error => {
-			runningThreadsCount--
-			const msg = `getChildrenToClone: Could not fetch the child documents of document with id ${payload.parentId} in database ${rootState.userData.currentDb}. ${error}`
-			dispatch('doLog', { event: msg, level: SEV.ERROR })
+			url: rootState.userData.currentDb + '/_design/design1/_view/docToParentMap?' + composeRangeString2(payload.parentId) + '&include_docs=true',
 		})
+			.then((res) => {
+				runningThreadsCount--
+				const results = res.data.rows
+				if (results.length > 0) {
+					// process next level
+					dispatch('processItemsToClone', {
+						originalNode: payload.originalNode,
+						cloneProductId: payload.cloneProductId,
+						results: results.map((r) => r.doc),
+						newParentId: payload.parentId,
+						cloneParentId: payload.cloneParentId,
+						cloneParentNode: payload.cloneParentNode,
+					})
+				} else {
+					if (runningThreadsCount === 0) {
+						// db iteration ready
+						dispatch('addHistToClonedDoc', payload)
+					}
+				}
+			})
+			.catch((error) => {
+				runningThreadsCount--
+				const msg = `getChildrenToClone: Could not fetch the child documents of document with id ${payload.parentId} in database ${rootState.userData.currentDb}. ${error}`
+				dispatch('doLog', { event: msg, level: SEV.ERROR })
+			})
 	},
 
 	/* Add history to the item that was cloned */
-	addHistToClonedDoc({
-		rootState,
-		dispatch
-	}, payload) {
+	addHistToClonedDoc({ rootState, dispatch }, payload) {
 		globalAxios({
 			method: 'GET',
-			url: rootState.userData.currentDb + '/' + payload.originalNode._id
-		}).then(res => {
-			const updatedDoc = res.data
-			const newHist = {
-				clonedBranchEvent: [payload.originalNode.level, payload.originalNode.subtype],
-				by: rootState.userData.user,
-				timestamp: Date.now(),
-				isListed: true,
-				distributeEvent: false
-			}
-			updatedDoc.history.unshift(newHist)
-
-			if (payload.cloneProductId) {
-				// copy the assigned roles
-				const userRoles = rootState.userData.myDatabases[rootState.userData.currentDb].productsRoles[payload.originalNode._id]
-				// update the current user's profile with the cloned product
-				const newProductOption = {
-					value: payload.cloneProductId,
-					text: clonedRootNode.title
-				}
-				dispatch('assignProductToUserAction', {
-					dbName: rootState.userData.currentDb, selectedUser: rootState.userData.user, newProductOption, userRoles, onFailureCallback: () => {
-						busyCloning = false
-					}
-				})
-			}
-
-			dispatch('updateDoc', {
-				dbName: rootState.userData.currentDb, updatedDoc, caller: 'addHistToClonedDoc', onSuccessCallback: () => {
-					dispatch('showBranch', payload)
-				}, onfailureCallback: () => {
-					busyCloning = false
-				}
-			})
-		}).catch(error => {
-			const msg = `addHistToClonedDoc: Could not read the document with id ${payload.originalNode._id} from database ${rootState.userData.currentDb}, ${error}`
-			dispatch('doLog', { event: msg, level: SEV.ERROR })
-			busyCloning = false
+			url: rootState.userData.currentDb + '/' + payload.originalNode._id,
 		})
+			.then((res) => {
+				const updatedDoc = res.data
+				const newHist = {
+					clonedBranchEvent: [payload.originalNode.level, payload.originalNode.subtype],
+					by: rootState.userData.user,
+					timestamp: Date.now(),
+					isListed: true,
+					distributeEvent: false,
+				}
+				updatedDoc.history.unshift(newHist)
+
+				if (payload.cloneProductId) {
+					// copy the assigned roles
+					const userRoles = rootState.userData.myDatabases[rootState.userData.currentDb].productsRoles[payload.originalNode._id]
+					// update the current user's profile with the cloned product
+					const newProductOption = {
+						value: payload.cloneProductId,
+						text: clonedRootNode.title,
+					}
+					dispatch('assignProductToUserAction', {
+						dbName: rootState.userData.currentDb,
+						selectedUser: rootState.userData.user,
+						newProductOption,
+						userRoles,
+						onFailureCallback: () => {
+							busyCloning = false
+						},
+					})
+				}
+
+				dispatch('updateDoc', {
+					dbName: rootState.userData.currentDb,
+					updatedDoc,
+					caller: 'addHistToClonedDoc',
+					onSuccessCallback: () => {
+						dispatch('showBranch', payload)
+					},
+					onfailureCallback: () => {
+						busyCloning = false
+					},
+				})
+			})
+			.catch((error) => {
+				const msg = `addHistToClonedDoc: Could not read the document with id ${payload.originalNode._id} from database ${rootState.userData.currentDb}, ${error}`
+				dispatch('doLog', { event: msg, level: SEV.ERROR })
+				busyCloning = false
+			})
 	},
 
 	/* Show the cloned nodes in the tree view and add an entry to undo the clone */
-	showBranch({
-		rootState,
-		commit,
-	}, payload) {
+	showBranch({ rootState, commit }, payload) {
 		const parentNode = rootState.helpersRef.getNodeById(payload.originalNode.parentId)
 		const locationInfo = getLocationInfo(clonedRootDoc.priority, parentNode)
 		// insert the cloned root node in the tree above the original
-		rootState.helpersRef.insertNodes({
-			nodeModel: locationInfo.prevNode,
-			placement: locationInfo.newInd === 0 ? 'inside' : 'after'
-		}, [clonedRootNode], { skipUpdateProductId: true })
+		rootState.helpersRef.insertNodes(
+			{
+				nodeModel: locationInfo.prevNode,
+				placement: locationInfo.newInd === 0 ? 'inside' : 'after',
+			},
+			[clonedRootNode],
+			{ skipUpdateProductId: true }
+		)
 		// select the cloned node
 		const nowSelectedNode = rootState.helpersRef.getNodeById(clonedRootDoc._id)
 		commit('updateNodesAndCurrentDoc', { selectNode: nowSelectedNode })
@@ -240,14 +266,19 @@ const actions = {
 		const entry = {
 			type: 'undoBranchClone',
 			newNode: clonedRootNode,
-			productWasCloned: payload.cloneProductId !== undefined
+			productWasCloned: payload.cloneProductId !== undefined,
 		}
 		rootState.changeHistory.unshift(entry)
-		commit('addToEventList', { txt: `The ${rootState.helpersRef.getLevelText(payload.originalNode.level)} '${payload.originalNode.title}' and ${clonedDocsCount - 1} descendants are cloned`, severity: SEV.INFO })
+		commit('addToEventList', {
+			txt: `The ${rootState.helpersRef.getLevelText(payload.originalNode.level)} '${payload.originalNode.title}' and ${
+				clonedDocsCount - 1
+			} descendants are cloned`,
+			severity: SEV.INFO,
+		})
 		busyCloning = false
-	}
+	},
 }
 
 export default {
-	actions
+	actions,
 }
