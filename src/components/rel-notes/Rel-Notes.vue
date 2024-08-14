4f4c7075efcbed7e7e1b336e04a7113e07868671
Erik Verheul <erik@verheulconsultants.nl>
2024-08-14 13:08:37 +0200

    Prepare for release

--- src/components/rel-notes/Rel-Notes.vue
+++ src/components/rel-notes/Rel-Notes.vue
@@ -6,6 +6,16 @@
         <BCol cols="12" class="left-column">
           <h1>RELEASE NOTES</h1>
           <p v-if="store.state.demo">Note: This is a demo version of the application.</p>
+          <h3>v.2.3.1 - 14 August 2024 Bug fix, refactoring</h3>
+          <p>Bug fix:</p>
+          <ul>
+            <li>Emails are only send to myself, other subscribed users are not notified: fixed</li>
+          </ul>
+          <p>Refactoring</p>
+          <ul>
+            <li>Make the mailing app ES6 module system compatible </li>
+            <li>Prettier formatter added and applied to all files</li>
+          </ul>
           <h3>v.2.3.0 - 13 August 2024 Bug fixex, refactoring, vulnerability fix</h3>
           <p>Bug fixes:</p>
           <ul>
--- src/store/store.js
+++ src/store/store.js
@@ -103,7 +103,7 @@ function getCurrentEvt(eventsArray, key) {
 const store = createStore({
 	state() {
 		return {
-			appVersion: '2.3.0',
+			appVersion: '2.3.1',
 			// generic helper functions
 			helpersRef: null,
 			// console log settings
