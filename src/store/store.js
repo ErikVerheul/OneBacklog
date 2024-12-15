db5c372f314619a9c18e315b8061fc239f0ad086
Erik Verheul <erik@verheulconsultants.nl>
2024-12-15 14:50:51 +0100

    prepare patch

--- src/store/store.js
+++ src/store/store.js
@@ -85,7 +85,7 @@ function getCurrentEvt(eventsArray, key) {
 const store = createStore({
 	state() {
 		return {
-			appVersion: '2.5.0',
+			appVersion: '2.5.0.1',
 			// generic helper functions
 			helpersRef: null,
 			// console log settings
