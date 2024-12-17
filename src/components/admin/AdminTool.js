1a82c601daea534b68d08e08f1b40b1579b82353
Erik Verheul <erik@verheulconsultants.nl>
2024-12-17 22:36:30 +0100

    Renamed files

---
+++ src/components/admin/AdminTool.js
@@ -0,0 +1,106 @@
+import { STATE, LEVEL, MISC } from '../../constants.js'
+import { uniTob64, createId } from '../../common_functions.js'
+import common_admin from './CommonAdmin.js'
+import store from '../../store/store.js'
+
+const methods = {
+	/* For all options the available databases are fetched once at mount */
+	createUser() {
+		this.optionSelected = 'Create a user and assign product(s)'
+		this.getUserFirst = false
+		this.userName = undefined
+		this.password = undefined
+		this.userEmail = undefined
+		this.credentialsReady = false
+		store.state.backendMessages = []
+		this.localMessage = ''
+		store.state.useracc.userIsAdmin = false
+		store.state.useracc.userIsAPO = false
+		store.state.isUserRemoved = false
+		store.state.isUserCreated = false
+		store.state.selectedDatabaseName = undefined
+	},
+
+	maintainUsers() {
+		this.optionSelected = 'Maintain user permissions to products'
+		this.getUserFirst = true
+		this.isUserDbSelected = false
+		this.canRemoveLastProduct = true
+		;(this.canRemoveDatabase = true), (this.localMessage = '')
+		store.state.backendMessages = []
+		store.state.isUserFound = false
+		store.state.areDatabasesFound = false
+		store.state.areProductsFound = false
+		store.state.isUserUpdated = false
+		store.state.selectedDatabaseName = undefined
+		// get the users to select from
+		store.dispatch('getAllUsers')
+	},
+
+	removeUser() {
+		this.optionSelected = 'Remove a user'
+		this.getUserFirst = true
+		store.state.isUserFound = false
+		this.userName = undefined
+		store.state.backendMessages = []
+		this.localMessage = ''
+		store.state.isUserDeleted = false
+		store.dispatch('getAllUsers')
+	},
+
+	createProduct() {
+		console.log('createProduct is called')
+		this.optionSelected = 'Create a product'
+		this.getUserFirst = false
+		this.productTitle = ''
+		store.state.isProductCreated = false
+		this.dbIsSelected = false
+		store.state.backendMessages = []
+	},
+
+	removeProduct() {
+		this.optionSelected = 'Remove a product'
+		this.getUserFirst = false
+		this.dbIsSelected = false
+	},
+
+	doCreateProduct() {
+		const _id = createId()
+		// a newly created product node will be inserted below the last product (see createProductAction in utils.js)
+		const lastProductNode = store.state.helpersRef.getRootNode().children.slice(-1)[0]
+		const priority = Math.floor((lastProductNode.data.priority + Number.MIN_SAFE_INTEGER) / 2)
+		// create a new document
+		const newProduct = {
+			_id,
+			type: 'backlogItem',
+			productId: _id,
+			parentId: 'root',
+			team: MISC.NOTEAM,
+			level: LEVEL.PRODUCT,
+			state: STATE.INPROGRESS,
+			reqarea: null,
+			title: this.productTitle,
+			followers: [],
+			description: uniTob64(MISC.EMPTYQUILL),
+			acceptanceCriteria: uniTob64('<p>Please do not neglect</p>'),
+			priority,
+			comments: [
+				{
+					ignoreEvent: 'comments initiated',
+					timestamp: Date.now(),
+				},
+			],
+		}
+		// update the database, insert the new node below the last product and add the product to this admin's subscriptions and productsRoles
+		store.dispatch('createProductAction', { dbName: store.state.selectedDatabaseName, newProduct, lastProductNode })
+	},
+
+	doRemoveUser() {
+		store.dispatch('removeUserIfExistent', this.selectedUser)
+	},
+}
+
+export default {
+	extends: common_admin,
+	methods,
+}
---
+++ src/components/header/AppHeader.js
@@ -0,0 +1,190 @@
+import router from '../../router'
+import Licence from './AppLicence.vue'
+import { isValidEmail } from '../../common_functions.js'
+import { authorization, utilities } from '../mixins/GenericMixin.js'
+import logo from '../../assets/logo.png'
+import store from '../../store/store.js'
+
+const MINPASSWORDLENGTH = 8
+
+// show browser dependant message 'Changes you made may not be saved. Stay on page / Leave page' when exiting the app irregularly
+const beforeUnloadHandler = (event) => {
+	event.preventDefault()
+}
+window.addEventListener('beforeunload', beforeUnloadHandler)
+
+function created() {
+	// add tag when signed in as demoUser
+	if (store.state.userData.user === 'demoUser') this.appVersion = this.appVersion + ' DEMO'
+}
+
+function data() {
+	return {
+		appVersion: store.state.appVersion,
+		logo: logo,
+		disableOkButton: false,
+		oldPassword: '',
+		newPassword1: '',
+		newPassword2: '',
+		newEmail1: '',
+		newEmail2: '',
+		selectedProducts: [],
+		selectedTeam: '',
+		headerMyDatabase: '',
+		headerDatabaseOptions: [],
+		teamOptions: [],
+		showChangeDatabase: false,
+		showOptionsModal: false,
+		showUserguide: false,
+		showSelectProducts: false,
+	}
+}
+
+const computed = {
+	canMessage() {
+		return true
+	},
+
+	canChangeDb() {
+		return store.state.userData.myOptions && store.state.userData.myOptions.proUser === 'true' && store.state.myAssignedDatabases.length > 1
+	},
+
+	emailIsCheckedOk() {
+		return this.newEmail1 === this.newEmail2 && isValidEmail(this.newEmail1)
+	},
+}
+
+const methods = {
+	saveMyOptions() {
+		// refreshPlanningboard to (un)show the OnHold column
+		this.refreshPlanningboard()
+		this.showOptionsModal = false
+		store.dispatch('saveMyOptionsAsync')
+	},
+
+	refreshPlanningboard() {
+		if (this.isPlanningBoardSelected)
+			store.dispatch('loadPlanningBoard', { sprintId: store.state.loadedSprintId, team: store.state.userData.myTeam, caller: 'header.refreshPlanningboard' })
+	},
+
+	showOptions() {
+		this.showOptionsModal = true
+	},
+
+	goMessage() {
+		store.state.showGoMessaging = true
+	},
+
+	changeDatabase() {
+		this.headerMyDatabase = store.state.userData.currentDb
+		this.headerDatabaseOptions = []
+		for (const db of store.state.myAssignedDatabases) {
+			this.headerDatabaseOptions.push(db)
+		}
+		this.showChangeDatabase = true
+	},
+
+	changeTeam() {
+		this.selectedTeam = this.myTeam
+		this.teamOptions = []
+		for (const team of Object.keys(store.state.allTeams)) {
+			this.teamOptions.push(team)
+		}
+		this.$refs.changeTeamRef.show()
+	},
+
+	showMyTeam() {
+		this.selectedTeam = this.myTeam
+		this.$refs.showTeamRef.show()
+	},
+
+	selectProducts() {
+		this.selectedProducts = this.getMyAssignedProductIds
+		this.showSelectProducts = true
+	},
+
+	changeMyPassword() {
+		if (this.isServerAdmin) {
+			alert("As a 'server admin' you cannot change your password here. Use Fauxton instead")
+		} else this.$refs.changePwRef.show()
+	},
+
+	changeMyEmail() {
+		;(this.newEmail1 = ''), (this.newEmail2 = ''), this.$refs.changeEmailRef.show()
+	},
+
+	showMyRoles() {
+		this.$refs.showMyRolesRef.show()
+	},
+
+	doChangeDatabase() {
+		if (this.headerMyDatabase !== store.state.userData.currentDb) {
+			window.removeEventListener('beforeunload', beforeUnloadHandler)
+			const autoSignOut = true
+			store.dispatch('changeCurrentDb', { dbName: this.headerMyDatabase, autoSignOut })
+		}
+	},
+
+	/* Move tasks to the new team where this user is assigned to; when done other actions are started to complete the team change */
+	doChangeTeam() {
+		store.dispatch('updateTasksToNewTeam', {
+			userName: store.state.userData.user,
+			oldTeam: store.state.userData.myTeam,
+			newTeam: this.selectedTeam,
+		})
+	},
+
+	/* Return if nothing is selected */
+	doSelectProducts() {
+		if (this.getMyProductSubscriptionIds.length > 0) {
+			window.removeEventListener('beforeunload', beforeUnloadHandler)
+			store.dispatch('updateMyProductSubscriptions', { productIds: this.selectedProducts })
+		}
+	},
+
+	getSelectButtonText() {
+		if (this.selectedProducts.length === 1) return 'Save and restart'
+		return 'Continue'
+	},
+
+	doChangeMyPassWord() {
+		if (this.oldPassword !== store.state.userData.password) {
+			alert('Your current password is incorrect. Please try again.')
+			return
+		}
+		if (this.newPassword1 !== this.newPassword2) {
+			alert('You entered two different new passwords. Please try again.')
+			return
+		}
+		if (this.newPassword1.length < MINPASSWORDLENGTH) {
+			alert('Your new password must be 8 characters or longer. Please try again.')
+			return
+		}
+		store.dispatch('changeMyPasswordAction', this.newPassword1)
+	},
+
+	doChangeMyEmail() {
+		store.dispatch('changeMyEmailAction', this.newEmail1)
+	},
+
+	switchToDetailProductView() {
+		router.push('/treeView')
+	},
+
+	onSignout() {
+		window.removeEventListener('beforeunload', beforeUnloadHandler)
+		store.state.signingOut = true
+		store.dispatch('saveMyTreeViewAsync', { doEndSession: true })
+	},
+}
+
+export default {
+	mixins: [authorization, utilities],
+	created,
+	data,
+	computed,
+	methods,
+	components: {
+		appLicence: Licence,
+	},
+}
