e05e21ee3b3eb2d1b1ab2fb8d80c45a636f0947d
Erik Verheul <erik@verheulconsultants.nl>
2025-02-02 15:56:43 +0100

    Improve comment

--- src/store/modules/sync.js
+++ src/store/modules/sync.js
@@ -217,7 +217,7 @@ const actions = {
 
 				reportOddTimestamp(lastHistObj, doc._id)
 				if (doc._id === rootState.currentDoc._id) {
-					// replace encoded text fields
+					// set default team and decode text fields
 					rootState.currentDoc = prepareDocForPresentation(doc)
 				}
 				if (rootState.debug) console.log('sync:update the tree with event ' + histEvent)
