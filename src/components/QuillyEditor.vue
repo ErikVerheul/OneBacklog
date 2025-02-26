4a4610cd4e5c69db329c875209838804a76f345c
Erik Verheul <erik@verheulconsultants.nl>
2025-02-26 21:23:08 +0100

    Add a listener that always emits a blur when navigating outside the editor

--- src/components/QuillyEditor.vue
+++ src/components/QuillyEditor.vue
@@ -3,33 +3,36 @@
 </template>
 
 <script setup>
-import { MISC } from '../constants.js'
-import { onMounted, ref } from 'vue'
-import Quill from 'quill'
-import { QuillyEditor } from 'vue-quilly'
-import 'quill/dist/quill.snow.css'
+  import { MISC } from '../constants.js'
+  import { defineEmits, onMounted, ref } from 'vue'
+  import Quill from 'quill'
+  import { QuillyEditor } from 'vue-quilly'
+  import 'quill/dist/quill.snow.css'
 
-const editor = ref()
-const content = ref(MISC.EMPTYQUILL)
+  const emit = defineEmits(['blur'])
+  const editor = ref()
+  const content = ref(MISC.EMPTYQUILL)
+  const options = ref({
+    theme: 'snow',
+    modules: {
+      toolbar: [
+        [{ header: [false, 1, 2, 3, 4, 5, 6] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+        ['link', 'image', 'code-block'],
+        ['clean'] // remove formatting button
+      ],
+    },
+    placeholder: 'Insert text here ...',
+    readOnly: false
+  })
 
-const options = ref({
-  theme: 'snow',
-  modules: {
-    toolbar: [
-      [{ header: [false, 1, 2, 3, 4, 5, 6] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-      ['link', 'image', 'code-block'],
-      ['clean'] // remove formatting button
-    ],
-  },
-  placeholder: 'Insert text here ...',
-  readOnly: false
-})
-
-onMounted(() => {
-  editor.value.initialize(Quill)
-})
+  onMounted(() => {
+    const quill = editor.value.initialize(Quill)
+    quill.root.addEventListener('blur', () => {
+      emit('blur', quill)
+    })
+  })
 
 </script>
