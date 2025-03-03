d366cee0c4837ef446f75c6214f4965501be368b
Erik Verheul <erik@verheulconsultants.nl>
2025-03-03 20:31:36 +0100

    Use the editor component application wide

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import router from './router'
 import store from './store/store'
 import mitt from 'mitt'
 import FontAwesomeIcon from './fa.config'
-import QuillyEditor from './components/QuillyEditor.vue'
+import QEditor from './components/editor/QEditor.vue'
 import { createBootstrap } from 'bootstrap-vue-next'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 // import the global css, see https://stackoverflow.com/questions/39438094/best-way-to-have-global-css-in-vuejs
-import '@/css/onebacklog.scss'
-import '@/css/onebacklog.css'
+import './css/onebacklog.scss'
+import './css/onebacklog.css'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL
 axios.defaults.withCredentials = true
@@ -31,7 +31,7 @@ app.use(router)
 app.use(store)
 app.use(createBootstrap())
 app.component('font-awesome-icon', FontAwesomeIcon)
-app.component('QuillyEditor', QuillyEditor)
+app.component('QEditor', QEditor)
 app.config.globalProperties.eventBus = eventBus
 // make the store available in all templates
 app.config.globalProperties.store = store
