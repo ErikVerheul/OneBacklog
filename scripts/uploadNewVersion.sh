1de49fae6d3530a0f64941cb46822705ad59e0fa
Erik Verheul <erik@verheulconsultants.nl>
2025-04-29 09:25:43 +0200

    Refactor slightly

--- scripts/uploadNewVersion.sh
+++ scripts/uploadNewVersion.sh
@@ -4,20 +4,19 @@
 # The script will copy the files to the target directory
 # The target directory is set in the .env file
 source .env
-TARGET_DIR=${TARGET_DIR}
 if [ -z "${TARGET_DIR}" ]; then
   echo "The target directory is not set in the .env file"
   echo "Please set the TARGET_DIR variable in the .env file"
   exit 1
 fi
-echo "The target directory is set to: $TARGET_DIR"
+echo "The target directory is set to: ${TARGET_DIR}"
 echo "A copy of the .env file will be uploaded to the server"
-scp .env $TARGET_DIR
+scp .env ${TARGET_DIR}
 echo "The files index.html, favicon.ico and maintenance.tml will be copied to your target directory:"
-scp dist/index.html dist/favicon.ico dist/maintenance.html $TARGET_DIR
+scp dist/index.html dist/favicon.ico dist/maintenance.html ${TARGET_DIR}
 echo "The folders assets and img will be copied to your target directory:"
-scp -r dist/assets dist/img $TARGET_DIR
+scp -r dist/assets dist/img ${TARGET_DIR}
 echo "Copy the e-mail service app to the mailservice subdirectory of the target directory"
-scp -r mailservice $TARGET_DIR
+scp -r mailservice ${TARGET_DIR}
 echo
 echo "Upload is done"
