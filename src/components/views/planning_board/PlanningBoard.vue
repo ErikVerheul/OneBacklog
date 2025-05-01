679152fd2c190b3c22f97dcda31d8709a563dc5e
Erik Verheul <erik@verheulconsultants.nl>
2025-05-01 17:06:48 +0200

    Try to force landscape view on small devices

--- src/components/views/planning_board/PlanningBoard.vue
+++ src/components/views/planning_board/PlanningBoard.vue
@@ -1,278 +1,303 @@
 <template>
-  <div>
-    <app-header>
-      <!-- Right aligned nav items -->
-      <BNavbarNav class="ml-auto">
-        <BNavText>Start: {{ getStartDateString }}</BNavText>
-        <div class="divider" />
-      </BNavbarNav>
+	<div>
+		<app-header>
+			<!-- Right aligned nav items -->
+			<BNavbarNav class="ml-auto">
+				<BNavText>Start: {{ getStartDateString }}</BNavText>
+				<div class="divider" />
+			</BNavbarNav>
 
-      <BNavbarNav>
-        <BNavForm>
-          <BFormSelect v-model="selectedSprint" :options="sprintTitleOptions"></BFormSelect>
-        </BNavForm>
-      </BNavbarNav>
+			<BNavbarNav>
+				<BNavForm>
+					<BFormSelect v-model="selectedSprint" :options="sprintTitleOptions"></BFormSelect>
+				</BNavForm>
+			</BNavbarNav>
 
-      <BNavbarNav>
-        <div class="divider" />
-        <BNavText>End: {{ getEndDateString }}</BNavText>
-      </BNavbarNav>
-    </app-header>
-    <BContainer fluid>
-      <BRow v-if="!showWarning" class="title-bar">
-        <BCol cols="5">
-          <h5>Welcome {{ userData.user }} from team '{{ selectedTeam }}'</h5>
-        </BCol>
-        <BCol cols="4">
-          <h5 v-if="getPersonHours > 0">{{ getStoryPoints }} story points and {{ getPersonHours }} hours for spikes in this sprint</h5>
-          <h5 v-else>{{ getStoryPoints }} story points in this sprint</h5>
-        </BCol>
-        <BCol cols="2">
-          <h5>points done: {{ getStoryPointsDone }}</h5>
-        </BCol>
-        <BCol cols="1">
-          <span class="messSquare" v-b-popover.hover.bottomright="'Click to do messaging'" :style="{ 'background-color': store.state.messSquareColor }"
-            @click="goMessaging">mess</span>
-          <span class="syncOLSquare" v-bind:style="{ 'background-color': squareColor }">{{ squareText }}</span>
-        </BCol>
-      </BRow>
-      <BRow v-else class="warning-bar">
-        <BCol cols="11">
-          <h5>{{ store.state.warningText }}</h5>
-        </BCol>
-        <BCol cols="1">
-          <BButton @click="clearWarning()" size="sm">Dismiss</BButton>
-        </BCol>
-      </BRow>
-      <div v-for="story in store.state.planningboard.stories" :key="story.idx">
-        <BRow>
-          <BCol cols="12">
-            <story-lane :idx="story.idx"></story-lane>
-          </BCol>
-        </BRow>
-      </div>
-    </BContainer>
-    <template v-if="currentSprintLoaded && askForImport() && unfinishedWork">
-      <BModal @cancel="$router.back()" @close="$router.back()" @ok="procSelected" v-model="currentSprintLoaded"
-        title="Import unfinished tasks from previous sprints?">
-        <BListGroup>
-          <BListGroupItem button :active="contextOptionSelected === MOVE_TASKS" variant="dark" @click="prepSelected(MOVE_TASKS)">Yes, please</BListGroupItem>
-          <BListGroupItem button :active="contextOptionSelected === NO_NOT_YET" variant="dark" @click="prepSelected(NO_NOT_YET)">No, not yet</BListGroupItem>
-          <BListGroupItem button :active="contextOptionSelected === NO_STOP_ASKING" variant="danger" @click="prepSelected(NO_STOP_ASKING)">No, and do not ask
-            again</BListGroupItem>
-        </BListGroup>
-        <p class="message-head">{{ showInfo() }}</p>
-      </BModal>
-    </template>
-  </div>
+			<BNavbarNav>
+				<div class="divider" />
+				<BNavText>End: {{ getEndDateString }}</BNavText>
+			</BNavbarNav>
+		</app-header>
+		<BContainer fluid>
+			<BRow v-if="!showWarning" class="title-bar">
+				<BCol cols="5">
+					<h5>Welcome {{ userData.user }} from team '{{ selectedTeam }}'</h5>
+				</BCol>
+				<BCol cols="4">
+					<h5 v-if="getPersonHours > 0">{{ getStoryPoints }} story points and {{ getPersonHours }} hours for spikes in this sprint</h5>
+					<h5 v-else>{{ getStoryPoints }} story points in this sprint</h5>
+				</BCol>
+				<BCol cols="2">
+					<h5>points done: {{ getStoryPointsDone }}</h5>
+				</BCol>
+				<BCol cols="1">
+					<span class="messSquare" v-b-popover.hover.bottomright="'Click to do messaging'" :style="{ 'background-color': store.state.messSquareColor }"
+						@click="goMessaging">mess</span>
+					<span class="syncOLSquare" v-bind:style="{ 'background-color': squareColor }">{{ squareText }}</span>
+				</BCol>
+			</BRow>
+			<BRow v-else class="warning-bar">
+				<BCol cols="11">
+					<h5>{{ store.state.warningText }}</h5>
+				</BCol>
+				<BCol cols="1">
+					<BButton @click="clearWarning()" size="sm">Dismiss</BButton>
+				</BCol>
+			</BRow>
+			<div v-for="story in store.state.planningboard.stories" :key="story.idx">
+				<BRow>
+					<BCol cols="12">
+						<story-lane :idx="story.idx"></story-lane>
+					</BCol>
+				</BRow>
+			</div>
+		</BContainer>
+		<template v-if="currentSprintLoaded && askForImport() && unfinishedWork">
+			<BModal @cancel="$router.back()" @close="$router.back()" @ok="procSelected" v-model="currentSprintLoaded"
+				title="Import unfinished tasks from previous sprints?">
+				<BListGroup>
+					<BListGroupItem button :active="contextOptionSelected === MOVE_TASKS" variant="dark" @click="prepSelected(MOVE_TASKS)">Yes, please</BListGroupItem>
+					<BListGroupItem button :active="contextOptionSelected === NO_NOT_YET" variant="dark" @click="prepSelected(NO_NOT_YET)">No, not yet</BListGroupItem>
+					<BListGroupItem button :active="contextOptionSelected === NO_STOP_ASKING" variant="danger" @click="prepSelected(NO_STOP_ASKING)">No, and do not ask
+						again</BListGroupItem>
+				</BListGroup>
+				<p class="message-head">{{ showInfo() }}</p>
+			</BModal>
+		</template>
+	</div>
 </template>
 
 <script>
-import { MISC } from '../../../constants.js'
-import { initMessaging } from '../../../common_functions.js'
-import { mapState, mapGetters } from 'vuex'
-import { utilities } from '../../mixins/GenericMixin.js'
-import AppHeader from '../../header/AppHeader.vue'
-import StoryLane from './StoryLane.vue'
-import store from '../../../store/store.js'
+	import { MISC } from '../../../constants.js'
+	import { initMessaging } from '../../../common_functions.js'
+	import { mapState, mapGetters } from 'vuex'
+	import { utilities } from '../../mixins/GenericMixin.js'
+	import AppHeader from '../../header/AppHeader.vue'
+	import StoryLane from './StoryLane.vue'
+	import store from '../../../store/store.js'
+	import { useScreenOrientation } from '@vueuse/core'
 
-export default {
-  mixins: [utilities],
+	const { isSupported, orientation, angle, lockOrientation, unlockOrientation } = useScreenOrientation()
 
-  beforeCreate() {
-    store.state.currentView = 'planningBoard'
-  },
+	export default {
+		mixins: [utilities],
 
-  created() {
-    this.MOVE_TASKS = 0
-    this.NO_NOT_YET = 1
-    this.NO_STOP_ASKING = 2
+		beforeCreate() {
+			store.state.currentView = 'planningBoard'
+		},
 
-    if (store.state.loadedSprintId) {
-      // preset the selected sprint to the last loaded sprint
-      for (const s of store.state.myCurrentSprintCalendar) {
-        if (s.id === store.state.loadedSprintId) {
-          this.selectedSprint = s
-          break
-        }
-      }
-    }
-    if (!this.selectedSprint) {
-      // preset the selected sprint to the current sprint
-      this.selectedSprint = this.getActiveSprints.currentSprint
-    }
-  },
+		created() {
+			this.MOVE_TASKS = 0
+			this.NO_NOT_YET = 1
+			this.NO_STOP_ASKING = 2
 
-  data() {
-    return {
-      contextOptionSelected: undefined,
-      selectedSprint: null,
-      currentSprintLoaded: false
-    }
-  },
+			if (store.state.loadedSprintId) {
+				// preset the selected sprint to the last loaded sprint
+				for (const s of store.state.myCurrentSprintCalendar) {
+					if (s.id === store.state.loadedSprintId) {
+						this.selectedSprint = s
+						break
+					}
+				}
+			}
+			if (!this.selectedSprint) {
+				// preset the selected sprint to the current sprint
+				this.selectedSprint = this.getActiveSprints.currentSprint
+			}
+		},
 
-  watch: {
-    // initially load the current sprint and reload when the user selects another sprint
-    selectedSprint: function (newVal) {
-      this.currentSprintLoaded = newVal.id === this.getActiveSprints.currentSprint.id
-      store.dispatch('loadPlanningBoard', { sprintId: newVal.id, team: this.selectedTeam, caller: 'watch: selectedSprint' })
-    },
+		beforeMount() {
+			if (!store.state.onLargeScreen) {
+				// lock the screen orientation to landscape for mobile devices
+				if (isSupported) {
+					console.log('Locking screen orientation to landscape-primary')
+					lockOrientation('landscape-primary')
+				} else
+					console.log('Screen orientation is not supported')
+			}
+		},
 
-    // initially load the current sprint for the user's team and reload when the user changes team
-    selectedTeam: function (newVal) {
-      this.selectedSprint = this.getActiveSprints.currentSprint
-      store.dispatch('loadPlanningBoard', { sprintId: this.selectedSprint.id, team: newVal, caller: 'watch: selectedTeam' })
-    }
-  },
+		beforeUnmount() {
+			// reset the loaded sprint id to 0 when leaving the planning board
+			store.state.loadedSprintId = 0
+			if (!store.state.onLargeScreen && isSupported) {
+				// lock the screen orientation to landscape for mobile devices
+				console.log('unLocking screen orientation')
+				unlockOrientation()
+			}
+		},
 
-  computed: {
-    ...mapGetters([
-      'getPersonHours',
-      'getStoryPoints',
-      'getStoryPointsDone'
-    ]),
-    ...mapState(['userData']),
+		data() {
+			return {
+				isSupported, orientation, angle,
+				contextOptionSelected: undefined,
+				selectedSprint: null,
+				currentSprintLoaded: false
+			}
+		},
 
-    selectedTeam() {
-      return this.userData.myTeam
-    },
+		watch: {
+			// initially load the current sprint and reload when the user selects another sprint
+			selectedSprint: function (newVal) {
+				this.currentSprintLoaded = newVal.id === this.getActiveSprints.currentSprint.id
+				store.dispatch('loadPlanningBoard', { sprintId: newVal.id, team: this.selectedTeam, caller: 'watch: selectedSprint' })
+			},
 
-    showWarning() {
-      return store.state.warningText !== ''
-    },
+			// initially load the current sprint for the user's team and reload when the user changes team
+			selectedTeam: function (newVal) {
+				this.selectedSprint = this.getActiveSprints.currentSprint
+				store.dispatch('loadPlanningBoard', { sprintId: this.selectedSprint.id, team: newVal, caller: 'watch: selectedTeam' })
+			}
+		},
 
-    unfinishedWork() {
-      return store.state.planningboard.itemIdsToImport.length > 0
-    },
+		computed: {
+			...mapGetters([
+				'getPersonHours',
+				'getStoryPoints',
+				'getStoryPointsDone'
+			]),
+			...mapState(['userData']),
 
-    getStartDateString() {
-      if (this.selectedSprint) return new Date(this.selectedSprint.startTimestamp).toString().substring(0, 33)
-      return ''
-    },
+			selectedTeam() {
+				return this.userData.myTeam
+			},
 
-    getEndDateString() {
-      if (this.selectedSprint) return new Date(this.selectedSprint.startTimestamp + this.selectedSprint.sprintLength).toString().substring(0, 33)
-      return ''
-    },
+			showWarning() {
+				return store.state.warningText !== ''
+			},
 
-    /* Return date/time dependent sprint selection options, recent first + next sprint on top*/
-    sprintTitleOptions() {
-      const now = Date.now()
-      const calendar = store.state.myCurrentSprintCalendar
-      const options = []
-      let getNextSprint = true
-      let getCurrSprint = true
-      for (let i = calendar.length - 1; i >= 0; i--) {
-        const sprint = calendar[i]
-        if (sprint.startTimestamp > now) continue
+			unfinishedWork() {
+				return store.state.planningboard.itemIdsToImport.length > 0
+			},
 
-        if (getNextSprint) {
-          const nextSprint = calendar[i + 1]
-          options.push({ value: nextSprint, text: nextSprint.name + ' (next sprint)' })
-          getNextSprint = false
-        }
-        if (getCurrSprint) {
-          const currSprint = sprint
-          options.push({ value: currSprint, text: currSprint.name + ' (current sprint)' })
-          getCurrSprint = false
-          continue
-        }
-        options.push({ value: sprint, text: `${sprint.name} Stories-Done: ${store.state.helpersRef.countStoriesInSprint(sprint.id)}` })
-      }
-      return options
-    },
+			getStartDateString() {
+				if (this.selectedSprint) return new Date(this.selectedSprint.startTimestamp).toString().substring(0, 33)
+				return ''
+			},
 
-    squareText() {
-      if (store.state.online) return 'sync'
-      return 'offl'
-    },
+			getEndDateString() {
+				if (this.selectedSprint) return new Date(this.selectedSprint.startTimestamp + this.selectedSprint.sprintLength).toString().substring(0, 33)
+				return ''
+			},
 
-    squareColor() {
-      if (store.state.nowSyncing || !store.state.online) {
-        return '#e6f7ff'
-      }
-      return MISC.SQUAREBGNDCOLOR
-    }
-  },
+			/* Return date/time dependent sprint selection options, recent first + next sprint on top*/
+			sprintTitleOptions() {
+				const now = Date.now()
+				const calendar = store.state.myCurrentSprintCalendar
+				const options = []
+				let getNextSprint = true
+				let getCurrSprint = true
+				for (let i = calendar.length - 1; i >= 0; i--) {
+					const sprint = calendar[i]
+					if (sprint.startTimestamp > now) continue
 
-  methods: {
-    goMessaging() {
-      initMessaging(store)
-    },
+					if (getNextSprint) {
+						const nextSprint = calendar[i + 1]
+						options.push({ value: nextSprint, text: nextSprint.name + ' (next sprint)' })
+						getNextSprint = false
+					}
+					if (getCurrSprint) {
+						const currSprint = sprint
+						options.push({ value: currSprint, text: currSprint.name + ' (current sprint)' })
+						getCurrSprint = false
+						continue
+					}
+					options.push({ value: sprint, text: `${sprint.name} Stories-Done: ${store.state.helpersRef.countStoriesInSprint(sprint.id)}` })
+				}
+				return options
+			},
 
-    clearWarning() {
-      store.state.warningText = ''
-    },
+			squareText() {
+				if (store.state.online) return 'sync'
+				return 'offl'
+			},
 
-    askForImport() {
-      if (!this.userData.doNotAskForImport) return true
-      return !this.userData.doNotAskForImport.includes(this.getActiveSprints.currentSprint.id)
-    },
+			squareColor() {
+				if (store.state.nowSyncing || !store.state.online) {
+					return '#e6f7ff'
+				}
+				return MISC.SQUAREBGNDCOLOR
+			}
+		},
 
-    showInfo() {
-      return `Found ${store.state.planningboard.itemIdsToImport.length} unfinished tasks in ${store.state.planningboard.itemIdsToImport.length} items to import`
-    },
+		methods: {
+			goMessaging() {
+				initMessaging(store)
+			},
 
-    prepSelected(idx) {
-      this.contextOptionSelected = idx
-    },
+			clearWarning() {
+				store.state.warningText = ''
+			},
 
-    procSelected() {
-      switch (this.contextOptionSelected) {
-        case this.MOVE_TASKS:
-          store.dispatch('importInSprint', this.getActiveSprints.currentSprint.id)
-          break
-        case this.NO_NOT_YET:
-          // do nothing
-          break
-        case this.NO_STOP_ASKING:
-          store.dispatch('registerMyNoSprintImport', this.getActiveSprints.currentSprint.id)
-          break
-      }
-    }
-  },
+			askForImport() {
+				if (!this.userData.doNotAskForImport) return true
+				return !this.userData.doNotAskForImport.includes(this.getActiveSprints.currentSprint.id)
+			},
 
-  name: 'PlanningBoard',
-  components: {
-    'app-header': AppHeader,
-    'story-lane': StoryLane
-  }
-}
+			showInfo() {
+				return `Found ${store.state.planningboard.itemIdsToImport.length} unfinished tasks in ${store.state.planningboard.itemIdsToImport.length} items to import`
+			},
+
+			prepSelected(idx) {
+				this.contextOptionSelected = idx
+			},
+
+			procSelected() {
+				switch (this.contextOptionSelected) {
+					case this.MOVE_TASKS:
+						store.dispatch('importInSprint', this.getActiveSprints.currentSprint.id)
+						break
+					case this.NO_NOT_YET:
+						// do nothing
+						break
+					case this.NO_STOP_ASKING:
+						store.dispatch('registerMyNoSprintImport', this.getActiveSprints.currentSprint.id)
+						break
+				}
+			}
+		},
+
+		name: 'PlanningBoard',
+		components: {
+			'app-header': AppHeader,
+			'story-lane': StoryLane
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
-.message-head {
-  margin: 10px;
-}
+	.message-head {
+		margin: 10px;
+	}
 
-.divider {
-  width: 15px;
-  height: auto;
-  display: inline-block;
-}
+	.divider {
+		width: 15px;
+		height: auto;
+		display: inline-block;
+	}
 
-.title-bar {
-  background-color: #408fae;
-  padding-top: 4px;
-}
+	.title-bar {
+		background-color: #408fae;
+		padding-top: 4px;
+	}
 
-.warning-bar {
-  background-color: orange;
-  padding-top: 4px;
-  padding-bottom: 4px;
-}
+	.warning-bar {
+		background-color: orange;
+		padding-top: 4px;
+		padding-bottom: 4px;
+	}
 
-.messSquare {
-  position: absolute;
-  right: 70px;
-  padding: 5px;
-  margin-bottom: 4px;
-}
+	.messSquare {
+		position: absolute;
+		right: 70px;
+		padding: 5px;
+		margin-bottom: 4px;
+	}
 
-.syncOLSquare {
-  float: right;
-  width: 42px;
-  padding: 5px;
-  margin-bottom: 4px;
-}
+	.syncOLSquare {
+		float: right;
+		width: 42px;
+		padding: 5px;
+		margin-bottom: 4px;
+	}
 </style>
