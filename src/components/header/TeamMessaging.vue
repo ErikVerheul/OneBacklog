27ee4ff00fca359f63c3721809d20bf103ae5c80
Erik Verheul <erik@verheulconsultants.nl>
2025-05-04 11:14:16 +0200

    Change to extra large

--- src/components/header/TeamMessaging.vue
+++ src/components/header/TeamMessaging.vue
@@ -1,63 +1,63 @@
 <template>
-  <BModal size="lg" v-model="store.state.showGoMessaging" no-footer title="View and send messages">
-    <BCard border-variant="primary" :header="getHeaderText()" header-bg-variant="dark" header-text-variant="white" align="center">
-      <label for="newMsgId">Message title</label>
-      <BFormInput id="newMsgId" v-model="store.state.newMsgTitle" placeholder="Your message must have a title" />
-      <p class="mt-3 mb-0">Your message</p>
-      <QEditor v-model=store.state.myNewMessage />
-      <BButton :disabled="canNotSave" class="mt-3" variant="primary" @click="saveMessage">{{ getSaveButtonText() }}</BButton>
-    </BCard>
-    <h2 class="mt-3">Your team messages</h2>
-    <Listings :selectedForView="'messages'"></Listings>
-  </BModal>
+	<BModal size="xl" v-model="store.state.showGoMessaging" no-footer title="View and send messages">
+		<BCard border-variant="primary" :header="getHeaderText()" header-bg-variant="dark" header-text-variant="white" align="center">
+			<label for="newMsgId">Message title</label>
+			<BFormInput id="newMsgId" v-model="store.state.newMsgTitle" placeholder="Your message must have a title" />
+			<p class="mt-3 mb-0">Your message</p>
+			<QEditor v-model=store.state.myNewMessage />
+			<BButton :disabled="canNotSave" class="mt-3" variant="primary" @click="saveMessage">{{ getSaveButtonText() }}</BButton>
+		</BCard>
+		<h2 class="mt-3">Your team messages</h2>
+		<Listings :selectedForView="'messages'"></Listings>
+	</BModal>
 </template>
 
 <script>
-  import store from '../../store/store.js'
-  import Listings from '../listings/CommonListings.vue'
+	import store from '../../store/store.js'
+	import Listings from '../listings/CommonListings.vue'
 
-  const computed = {
-    canNotSave: function () {
-      return store.state.newMsgTitle.trim() === ''
-    },
-  }
+	const computed = {
+		canNotSave: function () {
+			return store.state.newMsgTitle.trim() === ''
+		},
+	}
 
-  const methods = {
-    getHeaderText() {
-      if (store.state.replaceMessage) return 'Edit your recent message'
-      return 'Create a message for your team members'
-    },
+	const methods = {
+		getHeaderText() {
+			if (store.state.replaceMessage) return 'Edit your recent message'
+			return 'Create a message for your team members'
+		},
 
-    getSaveButtonText() {
-      if (store.state.replaceMessage) return 'Replace message'
-      return 'Send message'
-    },
+		getSaveButtonText() {
+			if (store.state.replaceMessage) return 'Replace message'
+			return 'Send message'
+		},
 
-    saveMessage() {
-      if (store.state.replaceMessage) {
-        store.dispatch('updateMyTeamMessageAction', {
-          dbName: store.state.userData.currentDb,
-          newTitle: store.state.newMsgTitle,
-          newMessage: store.state.myNewMessage,
-          timestamp: store.state.replaceMessageTimestamp
-        })
-      } else {
-        store.dispatch('saveMyTeamMessageAction', {
-          dbName: store.state.userData.currentDb,
-          newTitle: store.state.newMsgTitle,
-          newMessage: store.state.myNewMessage,
-        })
-      }
-    },
-  }
+		saveMessage() {
+			if (store.state.replaceMessage) {
+				store.dispatch('updateMyTeamMessageAction', {
+					dbName: store.state.userData.currentDb,
+					newTitle: store.state.newMsgTitle,
+					newMessage: store.state.myNewMessage,
+					timestamp: store.state.replaceMessageTimestamp
+				})
+			} else {
+				store.dispatch('saveMyTeamMessageAction', {
+					dbName: store.state.userData.currentDb,
+					newTitle: store.state.newMsgTitle,
+					newMessage: store.state.myNewMessage,
+				})
+			}
+		},
+	}
 
-  const components = {
-    Listings,
-  }
+	const components = {
+		Listings,
+	}
 
-  export default {
-    components,
-    computed,
-    methods,
-  }
+	export default {
+		components,
+		computed,
+		methods,
+	}
 </script>
