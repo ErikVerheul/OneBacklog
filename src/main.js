19315dfbfbd1950109f187ec74a07c22fc561bd5
Erik Verheul <erik@verheulconsultants.nl>
2026-04-26 22:15:25 +0200

    Mobile detection implemented

--- src/main.js
+++ src/main.js
@@ -32,4 +32,58 @@ app.config.globalProperties.eventBus = eventBus
 // make the store available in all templates
 app.config.globalProperties.store = store
 
+/*
+ * Note: Typically orientation locking is only enabled on mobile devices, and when the browser context is full screen.
+ * In Firefox and some other browsers, the Screen Orientation API may not work under
+ * certain conditions or without a secure context (i.e., it needs to be run over HTTPS or on localhost).
+ */
+function rotateToLandscape() {
+	if (screen.orientation && screen.orientation.lock) {
+		screen.orientation
+			.lock('landscape')
+			.then(() => {
+				if (store.state.debug) console.log('Viewport locked to landscape')
+			})
+			.catch((error) => {
+				console.error('Could not lock viewport to landscape:', error)
+			})
+	} else {
+		if (store.state.debug) console.log('Screen orientation API not supported')
+	}
+}
+
+function checkIfOnMobile() {
+	if ('userAgentData' in navigator) {
+		navigator.userAgentData
+			.getHighEntropyValues(['platform', 'uaFullVersion', 'mobile'])
+			.then((ua) => {
+				if (store.state.debug) console.log(`Platform: ${ua.platform}`)
+				if (store.state.debug) console.log(`Full Version: ${ua.uaFullVersion}`)
+				if (store.state.debug) console.log(`Is Mobile: ${ua.mobile}`)
+
+				if (ua.mobile) {
+					rotateToLandscape()
+				}
+			})
+			.catch((error) => {
+				console.error('Error retrieving user agent data:', error)
+			})
+	} else {
+		const userAgent = navigator.userAgent
+		const isMobile = /Mobi|Android/i.test(userAgent)
+
+		if (store.state.debug) console.log(`User Agent: ${userAgent}`)
+		if (store.state.debug) console.log(`Is Mobile: ${isMobile}`)
+
+		if (isMobile) {
+			store.state.onLargeScreen = false
+			rotateToLandscape()
+		} else store.state.onLargeScreen = true
+		if (store.state.debug) console.log('store.state.onLargeScreen: ' + store.state.onLargeScreen)
+	}
+}
+
+// Call the function to only run the planning board on mobile devices in landscape mode
+checkIfOnMobile()
+
 app.mount('#app')
